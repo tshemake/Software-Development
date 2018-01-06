0308b9be3b97c23ebdc0f99e6188efedd1186f09
analyst <tshemake@gmail.com>
2018-01-06 15:49:55 +0500

    73/4760/step_8

---
+++ stepik/73/4760/step_8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+ * Откройте терминал нажатием "Open Terminal"
+ * Скачайте в нем архив
+ * https://stepik.org/media/attachments/course73/quiz_archive.tar.gz
+ * в директорию /home/quiz (для этого вам пригодится команда wget,
+ * см. предыдущее занятие).
+ * Распакуйте этот архив с использованием ровно одной команды
+ * терминала.
+ * Нажмите кнопку "Submit".
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wget -O /home/quiz/quiz_archive.tar.gz https://stepik.org/media/attachments/course73/quiz_archive.tar.gz");
+            Console.WriteLine("tar xzf /home/quiz/quiz_archive.tar.gz");
+        }
+    }
+}
