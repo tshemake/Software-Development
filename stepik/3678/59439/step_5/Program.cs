d20b41e454dd796da24fe38d7daee95db75d4e7c
analyst <tshemake@gmail.com>
2018-01-07 01:36:02 +0500

    3678/59439/5

---
+++ stepik/3678/59439/step_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Главное преимущество SSH, по-сравнению с Telnet(история)?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Безопасность");
+        }
+    }
+}
