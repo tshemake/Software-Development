04ab8f4db41ce2d16f98e7a2fb100376b14388cb
analyst <tshemake@gmail.com>
2018-01-07 00:59:37 +0500

    3678/59433/7

---
+++ stepik/3678/59433/step_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Необходимо осуществить вход в удаленную систему Linux и выйти
+ * из нее. имя пользователя box, пароль box. Подключаемся по
+ * localhost. Использовать команды ssh и exit 
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh box@localhost");
+            Console.WriteLine("exit");
+        }
+    }
+}
