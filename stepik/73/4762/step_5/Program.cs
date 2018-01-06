5a82c0d30719e2f04705697eac99eaf096656be9
analyst <tshemake@gmail.com>
2018-01-06 15:57:18 +0500

    73/4762/step_5

---
+++ stepik/73/4762/step_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Предположим, что в файле  text.txt записаны строки, показанные
+ * среди вариантов ответа. Отметьте только те из них, которые
+ * выведет на экран команда  grep "world" text.txt.
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The world is not enough");
+            Console.WriteLine("The \"world\" is not enough");
+            Console.WriteLine("world");
+            Console.WriteLine("The beautifulworld is not enough");
+            Console.WriteLine("The beautiful-world is not enough");
+        }
+    }
+}
