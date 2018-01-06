9c1243a9e966fab60d929c0f70c40383969cf382
analyst <tshemake@gmail.com>
2018-01-07 00:42:55 +0500

    971/30401/step_1

---
+++ stepik/971/30401/step_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * http://pokeliga.com/pictures/sprites/small_art/144.png
+ */
+
+namespace step_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Articuno");
+        }
+    }
+}
