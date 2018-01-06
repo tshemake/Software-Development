8d717447dfc54e756ff5b7bacf894241170ba97a
analyst <tshemake@gmail.com>
2018-01-07 01:20:48 +0500

    3678/59438/2

---
+++ stepik/3678/59438/step_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Выберите корректные команды:
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("scp /home/The\\ Idiot bibl@10.10.0.5:/classics/Dostoevsky/");
+            Console.WriteLine("scp /home/ reader@10.10.0.5://Pushkin/poems/Monument");
+            Console.WriteLine("scp reader@10.10.0.5:”/Tolstoy/War and Peace” home/novels");
+        }
+    }
+}
