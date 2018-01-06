94d3958cdb4c837e3246119ad24ae1fd5b31c98d
analyst <tshemake@gmail.com>
2018-01-07 01:20:56 +0500

    3678/59438/3

---
+++ stepik/3678/59438/step_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Необходимо скопировать на удаленный сервер (не переходя на него)
+ * локальный текстовый документ с вашего компьютера в папку
+ * /home/public, в которой пользователь имеет права на запись.
+ * Используйте SCP. Имя пользователя удаленной системы vasya,
+ * пароль vasya. Удаленный сервер localhost. Файл pi_course_rools.txt 
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("scp ./pi_course_rools.txt vasya@localhost:/home/public");
+        }
+    }
+}
