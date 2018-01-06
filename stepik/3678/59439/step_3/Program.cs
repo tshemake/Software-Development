55ac8d67b92734688fb21dd275a4b25e266b0041
analyst <tshemake@gmail.com>
2018-01-07 01:31:37 +0500

    3678/59439/3

---
+++ stepik/3678/59439/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * В каком году ssh приняли, как интернет стандарт?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2006");
+        }
+    }
+}
