c36a3746164c3264f3dc69c33882abc92bc4dbd2
analyst <tshemake@gmail.com>
2018-01-06 15:44:06 +0500

    73/6047/step_5

---
+++ stepik/73/6047/step_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Какую опцию нужно указать команде wget, чтобы она не выводила
+ * никаких сообщений на экран (Resolving.., Connecting to.. и т.д.)?
+ *
+ * Подсказка: для ответа на этот вопрос вам понадобится справка по
+ * команде wget, которую легко можно получить, набрав man wget.
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-q или --quiet");
+        }
+    }
+}
