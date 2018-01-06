f1268535f3922218bacca9d635253c85196ae442
analyst <tshemake@gmail.com>
2018-01-06 15:49:52 +0500

    73/4760/step_7

---
+++ stepik/73/4760/step_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какой набор опций нужно указать программе tar, чтобы запаковать
+ * файлы в my_archive.tar.bz2?
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-cjf");
+        }
+    }
+}
