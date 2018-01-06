c8bc0072fd4ce8f5553522072e53f8a3a615d0fa
analyst <tshemake@gmail.com>
2018-01-07 00:59:24 +0500

    3678/59433/2

---
+++ stepik/3678/59433/step_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Выберите верный(ые) вариант(ы):
+ *
+ * SSH - это:
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Безопасный шелл.");
+            Console.WriteLine("Сетевой протокол.");
+        }
+    }
+}
