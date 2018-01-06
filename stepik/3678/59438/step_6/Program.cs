62059a0497a907fb48a6f2da745192c21759eac4
analyst <tshemake@gmail.com>
2018-01-07 01:21:03 +0500

    3678/59438/6

---
+++ stepik/3678/59438/step_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Необходимо сравнить используя ssh и diff два файла
+ * /home/box/local_file.txt /home/vasya/remote_file.txt.
+ * Пользователь для входа vasya, пароль vasya, сервер localhost.
+ * Результат сравнения необходимо занести в файл result.txt,
+ * который должен находиться в /home/box.
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh vasya@localhost \"cat /home/vasya/remote_file.txt\" | diff - local_file.txt > result.txt");
+        }
+    }
+}
