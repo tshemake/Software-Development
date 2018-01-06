c3ae8fb01fdab44f120e2016789d0ce4421b4d3e
analyst <tshemake@gmail.com>
2018-01-06 16:06:51 +0500

    73/4764/step_6

---
+++ stepik/73/4764/step_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Предположим, что вы устанавливаете программу program на свой
+ * компьютер при помощи команды sudo apt-get install program.
+ * Терминал сообщает вам, что он не может найти и скачать
+ * установочный пакет. Какие действия могут устранить проблему?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Проверка интернет соединения и его установка, если соединения нет.");
+            Console.WriteLine("sudo apt-get update");
+        }
+    }
+}
