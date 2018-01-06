f25f86d33497c68b749d594f6f8f3d8568883535
analyst <tshemake@gmail.com>
2018-01-06 15:40:37 +0500

    73/4759/step_4

---
+++ stepik/73/4759/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Куда по умолчанию выводится поток ошибок из программы,
+ * запущенной в терминале?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("На экран");
+        }
+    }
+}
