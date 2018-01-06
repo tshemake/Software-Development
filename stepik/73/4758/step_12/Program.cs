808b15f4fa221b391fc0ff5283c129ea56feebf0
analyst <tshemake@gmail.com>
2018-01-06 15:29:33 +0500

    73/4758/step_12

---
+++ stepik/73/4758/step_12/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какая команда используется для удаления директорий?
+ */
+
+namespace step_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("rm -r");
+        }
+    }
+}
