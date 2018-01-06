2d8adfe38452f78662f46750593a8bddb0704e2d
analyst <tshemake@gmail.com>
2018-01-06 16:25:18 +0500

    73/4765/step_10

---
+++ stepik/73/4765/step_10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * С помощью какой команды можно мгновенно завершить остановленный
+ * процесс?
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kill -9");
+        }
+    }
+}
