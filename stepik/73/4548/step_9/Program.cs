48b2f788607259b5bc21b3615f2721a67ad82595
analyst <tshemake@gmail.com>
2018-01-06 15:07:29 +0500

    73/4548/step_9

---
+++ stepik/73/4548/step_9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Смогли ли вы запустить на своем компьютере Linux?
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Да");
+        }
+    }
+}
