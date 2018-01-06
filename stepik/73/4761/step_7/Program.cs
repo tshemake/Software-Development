e46d358008c60f1f0a7bae16b3ad3bd67a79a36c
analyst <tshemake@gmail.com>
2018-01-06 15:37:07 +0500

    73/4761/step_7

---
+++ stepik/73/4761/step_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+/*
+ * Скачайте файл с программой, сделайте его исполняемым, запустите
+ * и скопируйте то, что он выведет на экран, в форму ниже.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            Console.WriteLine(currentTime);
+            int controlSum = currentTime.ToCharArray().Select(ch => (int)ch).Aggregate(0, (acc, x) => acc + x);
+            Console.WriteLine("Control sum: {0}", controlSum);
+        }
+    }
+}
+
