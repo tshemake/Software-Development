98377723780374079eb21595dba0cbf9e9faadab
analyst <tshemake@gmail.com>
2018-01-06 16:06:47 +0500

    73/4764/step_4

---
+++ stepik/73/4764/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какая команда скопирует на сервер (в домашнюю директорию) папку
+ * stepic вместе с содержимым ее самой и всех ее подпапок?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("scp -r stepic username@server:~/");
+        }
+    }
+}
