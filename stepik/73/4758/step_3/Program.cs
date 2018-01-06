c48381be68f5d16316c9d15e6f72ecf72586b1da
analyst <tshemake@gmail.com>
2018-01-06 15:29:04 +0500

    73/4758/step_3

---
+++ stepik/73/4758/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Выберите все синонимы для “командной строки”.
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Терминал");
+            Console.WriteLine("Консоль");
+        }
+    }
+}
