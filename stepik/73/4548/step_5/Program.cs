880196d27bfb2d426802ea5e919d805caf388de2
analyst <tshemake@gmail.com>
2018-01-06 15:07:11 +0500

    73/4548/step_5

---
+++ stepik/73/4548/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Какую операционную систему вы обычно используете? В таких типах
+ * задания (с галочками/чекбоксами/checkbox) вы можете выбирать
+ * несколько вариантов ответа (от 0 до всех)!
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Windows");
+        }
+    }
+}
