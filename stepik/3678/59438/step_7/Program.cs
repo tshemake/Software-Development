19bcbb0d97b62d56f8632646fa75e00885087e43
analyst <tshemake@gmail.com>
2018-01-07 01:21:07 +0500

    3678/59438/7

---
+++ stepik/3678/59438/step_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Необходимо скопировать текстовый документ c удаленного сервера
+ * на ваш компьютер в папку /home/box, который расположен в папке
+ * /home/erdos. Используйте SCP. Имя пользователя удаленной системы
+ * erdos, пароль erdos. Cервер localhost. Файл block_and_anton.txt.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("scp erdos@localhost:/home/erdos/block_and_anton.txt /home/box");
+        }
+    }
+}
