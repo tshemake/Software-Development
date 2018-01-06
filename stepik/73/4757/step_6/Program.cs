4b79130ca5f9156e42c5183ad836e6dc2ca01317
analyst <tshemake@gmail.com>
2018-01-06 15:19:04 +0500

    73/4757/step_6

---
+++ stepik/73/4757/step_6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какое расширение имеют установочные пакеты в Linux (Ubuntu)?
+ */
+ 
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("deb");
+        }
+    }
+}
