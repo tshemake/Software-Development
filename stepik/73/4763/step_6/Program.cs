f897cfc55f99784c8d8608c3465ca24613cd5d29
analyst <tshemake@gmail.com>
2018-01-06 16:01:26 +0500

    73/4763/step_6

---
+++ stepik/73/4763/step_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Предположим программа ssh-keygen создала вам два ключа:
+ * id_rsa и id_rsa.pub. Какой из этих ключей можно без опаски
+ * пересылать по интернету?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("id_rsa.pub");
+        }
+    }
+}
