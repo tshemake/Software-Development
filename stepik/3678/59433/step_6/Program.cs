1b7bfb60fac44d82f249d5ad911dcb95a179e899
analyst <tshemake@gmail.com>
2018-01-07 00:59:33 +0500

    3678/59433/6

---
+++ stepik/3678/59433/step_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * С помощью какой команды производится аутентификация пользователя
+ * на удаленной машине?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh");
+        }
+    }
+}
