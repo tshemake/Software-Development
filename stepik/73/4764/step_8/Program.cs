5dbc7e6596fc1f78d0e8b87fade5450999cf6413
analyst <tshemake@gmail.com>
2018-01-06 16:06:56 +0500

    73/4764/step_8

---
+++ stepik/73/4764/step_8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Для чего можно использовать программу Filezilla?
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для просмотра содержимого директорий на сервере");
+            Console.WriteLine("Для просмотра содержимого директорий на своем компьютере");
+            Console.WriteLine("Для копирования файлов со своего компьютера на сервер");
+        }
+    }
+}
