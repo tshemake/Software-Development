9dc71ce35218e52087dab5a6d068e921c8437184
analyst <tshemake@gmail.com>
2018-01-06 15:29:12 +0500

    73/4758/step_5

---
+++ stepik/73/4758/step_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какая команда напечатает в какой директории мы сейчас находимся?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Только pwd");
+        }
+    }
+}
