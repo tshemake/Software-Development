785905350a52697a96d9098cab5d5966ffc3f8fd
analyst <tshemake@gmail.com>
2018-01-06 18:37:00 +0500

    73/6281/step_7

---
+++ stepik/73/6281/step_7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * Вы можете скачать и изучить скрипты, которые мы показали в
+ * видеофрагменте: variables1.sh, variables2.sh.
+ *
+ * Какие из представленных ниже строк могут быть именами переменных
+ * в bash? Выберите все подходящие варианты!
+ *
+ * Подсказка: если все варианты ответов являются неверными, то не
+ * отмечайте ни один из них и нажимайте кнопку "Отправить"/"Submit".
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("variable_123");
+            Console.WriteLine("variable123");
+            Console.WriteLine("_variable");
+            Console.WriteLine("variable");
+            Console.WriteLine("__variable");
+        }
+    }
+}
