67d48618e906147bebae3a308f155f1aebe2cbcf
analyst <tshemake@gmail.com>
2018-01-06 16:14:56 +0500

    73/4769/step_6

---
+++ stepik/73/4769/step_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Как обычно можно вызвать справочную информацию о программе program?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("help program");
+            Console.WriteLine("man program");
+            Console.WriteLine("program --help (в некоторых программах бывает еще -help или -h)");
+        }
+    }
+}
