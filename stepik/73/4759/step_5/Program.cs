65122a313470ccdb54ba79a07143e6be09566e1e
analyst <tshemake@gmail.com>
2018-01-06 15:40:42 +0500

    73/4759/step_5

---
+++ stepik/73/4759/step_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Какие (какая) из команд создадут файл file.txt и запишут
+ * в него поток ошибок программы program? Считайте, что в момент
+ * запуска программы файл file.txt не существует.
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program 2> file.txt");
+            Console.WriteLine("program 2>> file.txt");
+        }
+    }
+}
