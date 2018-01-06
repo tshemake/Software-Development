9496746eb2f6be55c7f1b6ebcb5bdbb543f06ef4
analyst <tshemake@gmail.com>
2018-01-06 15:57:12 +0500

    73/4762/step_3

---
+++ stepik/73/4762/step_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Какая маска команды find НЕ найдет файл Alexey.jpeg?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*.jpg");
+            Console.WriteLine("alexey.*");
+            Console.WriteLine("*.?");
+        }
+    }
+}
