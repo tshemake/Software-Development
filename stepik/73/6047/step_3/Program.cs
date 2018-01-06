0973b0ef2138626c6af9f9a5935f894848e6159a
analyst <tshemake@gmail.com>
2018-01-06 15:44:00 +0500

    73/6047/step_3

---
+++ stepik/73/6047/step_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * В каком файле на диске окажется картинка, если для её скачивания
+ * были выполнены следующие команды?
+ *
+ * /cd /home/alex/
+ * wget -P /home/alex/Pictures -O 1.jpg http://example.com/example.jpg
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/home/alex/1.jpg");
+        }
+    }
+}
