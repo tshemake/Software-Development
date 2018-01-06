e4ee22e1c4fb1f8984e92142394ec59c7427ae59
analyst <tshemake@gmail.com>
2018-01-06 15:19:14 +0500

    73/4757/step_10

---
+++ stepik/73/4757/step_10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Для чего можно использовать приложение Update Manager?
+ */
+ 
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для обновления всей системы до новой версии");
+            Console.WriteLine("Для обновления ссылок в Software Center");
+            Console.WriteLine("Для обновления установленных программ");
+        }
+    }
+}
