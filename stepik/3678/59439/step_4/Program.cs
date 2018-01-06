2cc12562928f6f831c5ed69883aafb1a5b55472d
analyst <tshemake@gmail.com>
2018-01-07 01:35:57 +0500

    3678/59439/4

---
+++ stepik/3678/59439/step_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Для аутентификации сервера в SSH используется протокол
+ * аутентификации сторон на основе алгоритмов?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Парольная аутентификация");
+            Console.WriteLine("DSA");
+            Console.WriteLine("Алгоритм цифровой подписи RSA");
+        }
+    }
+}
