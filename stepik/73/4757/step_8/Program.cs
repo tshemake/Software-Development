38bc4a85558f61517ecb194759fad591ad6e22f5
analyst <tshemake@gmail.com>
2018-01-06 15:19:10 +0500

    73/4757/step_8

---
+++ stepik/73/4757/step_8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Поставьте себе в систему плеер VLC (любым способом: через
+ * Software Center или скачиванием установочного пакета с сайта VLC).
+ *
+ * Запустите, откройте Help → About (или Shift+F1) и напишите ниже
+ * первую фамилию (без имени!) из вкладки Authors. Обратите внимание,
+ * что в англоязычных текстах обычно имя стоит на первом месте
+ * (first name), а фамилия на втором (last name).
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Denis-Courmont");
+        }
+    }
+}
