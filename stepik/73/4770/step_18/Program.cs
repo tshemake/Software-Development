c2df0b887f176d8cbaf80f9e930899539397e12e
analyst <tshemake@gmail.com>
2018-01-06 17:21:11 +0500

    73/4770/step_18

---
+++ stepik/73/4770/step_18/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Задание на самостоятельное изучение tmux. 
+ *
+ * Изучите справку по tmux (например, man tmux) и выберите из
+ * предложенных ниже tmux-команд ту, которая отвечает за
+ * переименование текущей вкладки.
+ */
+
+namespace step_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ctrl+B и , (запятая)");
+        }
+    }
+}
