8c02bfcd22de70550101ac9126b0123e1bc1be76
analyst <tshemake@gmail.com>
2018-01-06 15:49:45 +0500

    73/4760/step_5

---
+++ stepik/73/4760/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Какие из перечисленных программ-архиваторов могут создать архив
+ * из директории с файлами?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zip");
+            Console.WriteLine("tar");
+        }
+    }
+}
