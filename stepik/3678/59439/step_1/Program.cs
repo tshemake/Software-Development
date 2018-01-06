bca12cf3047b85295c1663ce2504063d3f36baef
analyst <tshemake@gmail.com>
2018-01-07 01:31:30 +0500

    3678/59439/1

---
+++ stepik/3678/59439/step_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Что позволяет производить ssh?
+ */
+
+namespace step_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("удалённое управление операционной системой");
+            Console.WriteLine("передавать данные");
+        }
+    }
+}
