d5cee4c8590051ea1d96e28c650488bf8dc56a51
analyst <tshemake@gmail.com>
2018-01-06 15:37:03 +0500

    73/4761/step_6

---
+++ stepik/73/4761/step_6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Чему эквивалентен запуск программы с &?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запуск, Ctrl+Z, bg");
+        }
+    }
+}
