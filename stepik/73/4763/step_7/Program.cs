8458cd511b57c8623ea6783ff806f2172d11d9b6
analyst <tshemake@gmail.com>
2018-01-06 16:01:29 +0500

    73/4763/step_7

---
+++ stepik/73/4763/step_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+ * Для начала выполнения нажмите кнопку "Open Terminal".
+ * Открывшийся в браузере терминал будем называть "локальный".
+ * Зайдите с этого терминала по SSH на удаленный сервер 10.31.2.1.
+ * Для доступа к серверу используйте логин quiz и пароль supersecret.
+ * Прочитайте содержимое файла /srv/files_on_server/secret на
+ * удалённом сервере и запишите его в локальный файл /home/quiz/secret
+ * (т.е. вам нужно будет выйти с сервера!). Для записи данных в файл
+ * воспользуйтесь командой:
+ *
+ * echo "SECRET TEXT" > /home/quiz/secret
+ * где вместо SECRET TEXT нужно указать текст из заданного файла.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh quiz@10.31.2.1 \"cat /srv/files_on_server/secret\" > /home/quiz/secret");
+        }
+    }
+}
