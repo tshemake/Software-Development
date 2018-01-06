69625ea1bf5ed0e9bc77dbe5924361a171efcd79
analyst <tshemake@gmail.com>
2018-01-06 17:20:50 +0500

    73/4770/step_10

---
+++ stepik/73/4770/step_10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Предположим, что в tmux осталась последняя открытая вкладка.
+ Что произойдет, если вы введете в этой вкладке в командную строку
+ команду exit?
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tmux завершит работу");
+        }
+    }
+}
