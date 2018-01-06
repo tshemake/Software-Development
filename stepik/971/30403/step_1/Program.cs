0df51830ab04443c5364e7298a51984862d43b8f
analyst <tshemake@gmail.com>
2018-01-07 00:42:55 +0500

    971/30403/step_1

---
+++ stepik/971/30403/step_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * http://pokeliga.com/pictures/sprites/small_art/62.png
+ */
+
+namespace step_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Poliwrath");
+        }
+    }
+}
