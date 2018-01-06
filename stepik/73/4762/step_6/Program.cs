065ce55da4b11bd1b14956003beb4d2ae4743742
analyst <tshemake@gmail.com>
2018-01-06 15:57:22 +0500

    73/4762/step_6

---
+++ stepik/73/4762/step_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+ * Cкачайте архив с произведениями Шекспира. Вам нужно сгенерировать
+ * файл, в котором будут все строчки из этих произведений, содержащие
+ * “love”, и загрузить этот файл в форму.
+ *
+ * Подсказка: для того, чтобы результаты поиска записались сразу в
+ * файл, можно воспользоваться перенаправлением вывода (см. занятие
+ * Ввод/Вывод).
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wget -O shakespeare.tar.gz https://stepik.org/media/attachments/course73/shakespeare.tar.gz");
+            Console.WriteLine("tar -xzf shakespeare.tar.gz");
+            Console.WriteLine("grep -r \"love\" Shakespeare/* > result.txt");
+        }
+    }
+}
