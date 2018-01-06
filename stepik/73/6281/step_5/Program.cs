a2c6c1b2e96b097b1f0fee27e4fe22dda9501117
analyst <tshemake@gmail.com>
2018-01-06 18:36:57 +0500

    73/6281/step_5

---
+++ stepik/73/6281/step_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+ * Вы можете скачать и изучить скрипты, которые мы показали в
+ * видеофрагменте: script1.sh, script2.sh.
+ *
+ * Предположим, что вы находитесь в директории /home/bi/Documents/
+ * и запускаете в ней скрипт следующего содержания:
+ *
+ * #!/bin/bash
+ *
+ * cd /home/bi/
+ * touch file1.txt
+ * cd /home/bi/Desktop/
+ * Как будет выглядеть абсолютный путь до созданного файла file1.txt
+ * по окончанию работы скрипта?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/home/bi/file1.txt");
+        }
+    }
+}
