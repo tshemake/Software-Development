2eb4f8edc47754e1e15d524c278b0595a1d88c7f
analyst <tshemake@gmail.com>
2018-01-07 01:21:00 +0500

    3678/59438/5

---
+++ stepik/3678/59438/step_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С использованием какой команды можно сравнить два файла?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("diff");
+        }
+    }
+}
