571b30792429fb1757a45f0a4b3167a4ac47613e
analyst <tshemake@gmail.com>
2018-01-07 21:29:28 +0500

    191/13192/step_3

---
+++ stepik/191/13192/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Для чего мошенники выманивают номер телефона?
+ */
+ 
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("чтобы украсть деньги с мобильного счёта;");
+        }
+    }
+}
