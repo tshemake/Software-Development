009be53b6b88396bb973831ea01d7209943793f3
analyst <tshemake@gmail.com>
2018-01-08 00:48:29 +0500

    3678/60135/step_5

---
+++ stepik/3678/60135/step_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Зачем используется ключик -p?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("port . Port to connect to on the remote host.");
+        }
+    }
+}
