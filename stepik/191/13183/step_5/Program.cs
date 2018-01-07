d2dee0fa12bc0c99fa641fdb3451d67be1d7da55
analyst <tshemake@gmail.com>
2018-01-07 19:56:16 +0500

    191/13183/step_5

---
+++ stepik/191/13183/step_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Почему не рекомендуется вводить пароли от аккаунтов в публичных
+ * wi-fi сетях?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("все ответы верны");
+        }
+    }
+}
