5327831d5aa0b2d8acfec538702129a3eb3acd11
analyst <tshemake@gmail.com>
2018-01-08 00:10:53 +0500

    3678/59502/step_5

---
+++ stepik/3678/59502/step_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Необходимо удалить 3 строчку в файле /home/vasya/remote_file.txt.
+ * Пользователь vasya, пароль vasya, сервер localhost.
+ *
+ * Подсказка: наиболее простой способ решения данной задачи
+ * предполагает использование vim.
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh vasya@localhost 'sed 3d -i /home/vasya/remote_file.txt'");
+        }
+    }
+}
