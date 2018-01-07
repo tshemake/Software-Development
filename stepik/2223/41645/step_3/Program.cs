580b2ca27429e7b08418f6fbef29906d81279cad
analyst <tshemake@gmail.com>
2018-01-08 01:02:30 +0500

    2223/41645/step_3

---
+++ stepik/2223/41645/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Кто является владельцем торговой марки JavaScript?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oracle Corporation");
+        }
+    }
+}
