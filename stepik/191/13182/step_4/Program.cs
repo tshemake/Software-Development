5902d2663289cabbf9099d8d4e45d46b3ea69574
analyst <tshemake@gmail.com>
2018-01-07 19:53:12 +0500

    191/13182/step_4

---
+++ stepik/191/13182/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Что есть у любого аккаунта?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("логин и пароль");
+        }
+    }
+}
