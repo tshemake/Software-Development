4ce95e5c4ba2c418c0e9dab5939d9eed7ca7a3f6
analyst <tshemake@gmail.com>
2018-01-07 22:59:15 +0500

    3678/59447/step_4

---
+++ stepik/3678/59447/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С использование какой команды происходит генерация ключа для ssh?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh-keygen");
+        }
+    }
+}
