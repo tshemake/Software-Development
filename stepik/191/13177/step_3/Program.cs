f0393013bcb4d3daf6fd59d37ddb9f9344217a3c
analyst <tshemake@gmail.com>
2018-01-07 19:37:07 +0500

    191/13177/step_3

---
+++ stepik/191/13177/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Что чаще всего интересует злоумышленников в интернете?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("данные банковских карт и счетов");
+            Console.WriteLine("список контактов, чтобы рассылать спам от имени пользователя");
+        }
+    }
+}
