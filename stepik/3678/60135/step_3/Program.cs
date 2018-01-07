a3a6057f55e4cc4f3d3a76cbc002af6d7abeee34
analyst <tshemake@gmail.com>
2018-01-08 00:48:23 +0500

    3678/60135/step_3

---
+++ stepik/3678/60135/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Зачем используется ключ -E при использовании ssh?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("log_file. Append debug logs to log_file instead of standard error.");
+        }
+    }
+}
