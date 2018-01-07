6f218f5645b9ca86f1ac4fe14acd7406c373d80e
analyst <tshemake@gmail.com>
2018-01-08 00:23:38 +0500

    3678/59505/step_2

---
+++ stepik/3678/59505/step_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * В каком случае возможно возникновение данной ошибки?
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("при миграции ssh ключей на новый компьютер");
+            Console.WriteLine("при случайной замене прав на ключи");
+        }
+    }
+}
