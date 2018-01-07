da3885d4ba48711381f93b3ffacb73321060c35e
analyst <tshemake@gmail.com>
2018-01-07 23:56:26 +0500

    3678/59501/step_8

---
+++ stepik/3678/59501/step_8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Как сохранить ключ в агенте?(команда)
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh-add");
+        }
+    }
+}
