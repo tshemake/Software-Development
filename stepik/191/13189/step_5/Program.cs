127da532b8630390205f3bb2edc5d13d7e797002
analyst <tshemake@gmail.com>
2018-01-07 21:28:39 +0500

    191/13189/step_5

---
+++ stepik/191/13189/step_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какой интернет-протокол обеспечивает безопасный просмотр сайтов?
+ * (Первые буквы в адресной строке браузера.)
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("https://");
+        }
+    }
+}
