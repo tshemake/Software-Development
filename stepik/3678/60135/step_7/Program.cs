100cb3640303fbdae8136e143de048826f4a632d
analyst <tshemake@gmail.com>
2018-01-08 00:48:36 +0500

    3678/60135/step_7

---
+++ stepik/3678/60135/step_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какой протокол аутентификации установлен по-умолчанию для
+ * OpenSSH SSH клиента?
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("second protocol version");
+        }
+    }
+}
