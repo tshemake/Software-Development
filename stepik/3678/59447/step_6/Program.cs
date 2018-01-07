7172a19e68452087f5e805206f9675afecf46bc9
analyst <tshemake@gmail.com>
2018-01-07 22:59:19 +0500

    3678/59447/step_6

---
+++ stepik/3678/59447/step_6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какой ключ копируют на сервер?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("открытый");
+        }
+    }
+}
