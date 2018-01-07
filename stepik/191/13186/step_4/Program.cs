7b2a0d3f49ccaa2b84fd87234a591c6c8b914d1c
analyst <tshemake@gmail.com>
2018-01-07 21:27:24 +0500

    191/13186/step_4

---
+++ stepik/191/13186/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Что нужно мошенникам, чтобы выманивать деньги у обычных
+ * пользователей?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("достаточно простой хитрости;");
+        }
+    }
+}
