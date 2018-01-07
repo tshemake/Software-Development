fedd1503f62cf4be16e2f630842353813876b7fd
analyst <tshemake@gmail.com>
2018-01-08 00:10:45 +0500

    3678/59502/step_2

---
+++ stepik/3678/59502/step_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Какие привилегии привносит ssh для работы с удаленной машиной
+ * при достаточных правах?
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Удаление файлов и директорий");
+            Console.WriteLine("Создание файлов и директорий");
+            Console.WriteLine("изменение файлов");
+            Console.WriteLine("Запуск программ");
+        }
+    }
+}
