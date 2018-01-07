72b37b62e65e1a574fa5dc21ebdce94efc0bc31c
analyst <tshemake@gmail.com>
2018-01-07 21:30:37 +0500

    191/13196/step_3

---
+++ stepik/191/13196/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какую информацию о банковской карточке ни в коем случае нельзя
+ * сообщать другим людям?
+ */
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("пин-код и CVV;");
+        }
+    }
+}
