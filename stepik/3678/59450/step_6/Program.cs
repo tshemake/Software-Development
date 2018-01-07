e61d47aee8ecb6f20c8ee3e6bed52f259bb43b41
analyst <tshemake@gmail.com>
2018-01-07 23:20:55 +0500

    3678/59450/step_6

---
+++ stepik/3678/59450/step_6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какой командой можно удалить известный ключ с сервера?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh-keygen -R server");
+        }
+    }
+}
