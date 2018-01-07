847e897bbba722dc49f314917ba3f28d0d858d2b
analyst <tshemake@gmail.com>
2018-01-07 13:55:00 +0500

    73/4767/step_3

---
+++ stepik/73/4767/step_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Пусть в директории /home/bi лежат файлы Star_Wars.avi, star_trek
+ * OST.mp3, STARS.txt, stardust.mpeg, Eddard_Stark_biography.txt.
+ *
+ * Отметьте все файлы, которые найдет команда
+ * find /home/bi -iname "star*", но НЕ найдет команда
+ * find /home/bi -name "star*"?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("STARS.txt");
+            Console.WriteLine("Star_Wars.avi");
+        }
+    }
+}
