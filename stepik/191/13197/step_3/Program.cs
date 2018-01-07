5a2b853893c258b1c94cba6d61f305f89e870f2e
analyst <tshemake@gmail.com>
2018-01-07 21:31:01 +0500

    191/13197/step_3

---
+++ stepik/191/13197/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Главная защита электронного кошелька — это?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("платёжный пароль;");
+        }
+    }
+}
