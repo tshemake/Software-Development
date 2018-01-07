9189523c1fea1c12170c8bb5a74b0266e1634d57
analyst <tshemake@gmail.com>
2018-01-07 21:27:42 +0500

    191/13187/step_3

---
+++ stepik/191/13187/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какой платёжный пароль самый надёжный?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("одноразовый, он действует очень недолго, и его бессмысленно красть;");
+        }
+    }
+}
