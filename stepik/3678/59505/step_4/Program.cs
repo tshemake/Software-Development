fcbdae6f38fc08e01995e4556992a30f7d666da2
analyst <tshemake@gmail.com>
2018-01-08 00:23:42 +0500

    3678/59505/step_4

---
+++ stepik/3678/59505/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Суть причины возникновения ошибки?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("изменение прав на ключи ssh");
+        }
+    }
+}
