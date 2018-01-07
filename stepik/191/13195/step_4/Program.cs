96c2629aa0f25217860d2ddcb35667f6b4e6dd72
analyst <tshemake@gmail.com>
2018-01-07 21:30:18 +0500

    191/13195/step_4

---
+++ stepik/191/13195/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Как проверить репутацию фонда?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("навести справки о фонде на известном портале о благотворительности;");
+        }
+    }
+}
