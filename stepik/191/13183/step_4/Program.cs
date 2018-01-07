54826537507854b2932269a28b474e8d9224f247
analyst <tshemake@gmail.com>
2018-01-07 19:56:13 +0500

    191/13183/step_4

---
+++ stepik/191/13183/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Что такое брутфорс?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("подбор пароля");
+        }
+    }
+}
