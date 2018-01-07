819be16f026cce638dad24ef621912e4d1d5754a
analyst <tshemake@gmail.com>
2018-01-07 17:15:39 +0500

    73/6283/step_7

---
+++ stepik/73/6283/step_7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Отметьте какие характеристики файла можно посчитать с
+ * использованием команды wc.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Размер файла в байтах");
+            Console.WriteLine("Количество строк");
+            Console.WriteLine("Количество слов");
+            Console.WriteLine("Количество символов");
+            Console.WriteLine("Длину самой длинной строки");
+        }
+    }
+}
