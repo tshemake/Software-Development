643d0e89797181856b5f6d387fb7daa3099af5d5
analyst <tshemake@gmail.com>
2018-01-07 19:41:03 +0500

    191/13179/step_3

---
+++ stepik/191/13179/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Обязательно ли обновлять операционную систему?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("да, в новых версиях устраняются ошибки и уязвимости");
+        }
+    }
+}
