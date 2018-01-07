25514dad8bbb2df57f78b7965951025e506b25dd
analyst <tshemake@gmail.com>
2018-01-07 19:58:13 +0500

    191/13184/step_4

---
+++ stepik/191/13184/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Как узнать, что сайт, на который вы переходите, защищен?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ссылка на сайт начинается с https");
+        }
+    }
+}
