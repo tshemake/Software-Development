5c4112b540a10667d5d2fdf6d65af7cedb90b2d9
analyst <tshemake@gmail.com>
2018-01-07 23:29:28 +0500

    3678/59451/step_2

---
+++ stepik/3678/59451/step_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С использование какой команды можно изменить парольную фразу?
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh-keygen -p");
+        }
+    }
+}
