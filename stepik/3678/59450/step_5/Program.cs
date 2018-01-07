5b1f69481dd61b9ac3d587e87042f79c6c2d0e26
analyst <tshemake@gmail.com>
2018-01-07 23:20:51 +0500

    3678/59450/step_5

---
+++ stepik/3678/59450/step_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Для чего используется каталог known_hosts?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("хранение доверенных ключей");
+        }
+    }
+}
