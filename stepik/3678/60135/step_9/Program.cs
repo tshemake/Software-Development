1aa7ce860d33d295e99a99321cb269c6f85dd0d9
analyst <tshemake@gmail.com>
2018-01-08 00:48:42 +0500

    3678/60135/step_9

---
+++ stepik/3678/60135/step_9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Что хранится в /etc/ssh/ssh_known_hosts? (Первое предложение
+ * из документации, включая точку)
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Systemwide list of known host keys.");
+        }
+    }
+}
