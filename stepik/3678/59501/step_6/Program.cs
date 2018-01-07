4e8ef55d10a5e485ed6f7c5e7aa49daa260b2f0c
analyst <tshemake@gmail.com>
2018-01-07 23:56:22 +0500

    3678/59501/step_6

---
+++ stepik/3678/59501/step_6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * как завершить ssh-agent?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh-agent -k");
+        }
+    }
+}
