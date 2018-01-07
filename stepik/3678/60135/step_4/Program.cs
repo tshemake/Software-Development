47b70e83b7b98e0972905540f5b63d73064ea149
analyst <tshemake@gmail.com>
2018-01-08 00:48:27 +0500

    3678/60135/step_4

---
+++ stepik/3678/60135/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Зачем используется ключ -e при использовании ssh?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("escape_char. Sets the escape character for sessions with a pty");
+        }
+    }
+}
