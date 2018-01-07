2f5b61c63beff76d75965b560214dc7d5a1640a3
analyst <tshemake@gmail.com>
2018-01-07 19:37:36 +0500

    191/13178/step_4

---
+++ stepik/191/13178/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Чем трояны отличаются от вирусов?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("трояны распространяют люди, а вирусы распространяются самостоятельно");
+        }
+    }
+}
