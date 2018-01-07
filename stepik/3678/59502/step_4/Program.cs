8dea5325e8e896420820089a17df67f3b80a3230
analyst <tshemake@gmail.com>
2018-01-08 00:10:50 +0500

    3678/59502/step_4

---
+++ stepik/3678/59502/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Назовите один из наиболее популярный свободно распространяемых
+ * SSH-серверов.
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OpenSSH");
+        }
+    }
+}
