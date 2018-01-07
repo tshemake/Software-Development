740c6c803cbdcee2a6cdc58fd86a3512fcfd50fa
analyst <tshemake@gmail.com>
2018-01-08 00:27:24 +0500

    3678/59520/step_2

---
+++ stepik/3678/59520/step_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Является ли SSH платформа зависимым протоколом?
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("нет");
+        }
+    }
+}
