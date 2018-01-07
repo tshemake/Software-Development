8528fb5c9922bc83ce859529738e186586179037
analyst <tshemake@gmail.com>
2018-01-07 13:55:04 +0500

    73/4767/step_4

---
+++ stepik/73/4767/step_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Задание на понимание работы опций -path и -name команды find.
+ * Отметьте все верные утверждения из перечисленных ниже.
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("В некоторых случаях find с -name найдет больше файлов, чем find с таким же запросом, но с -path");
+            Console.WriteLine("В некоторых случаях find с -name найдет меньше файлов, чем find с таким же запросом, но с -path");
+        }
+    }
+}
