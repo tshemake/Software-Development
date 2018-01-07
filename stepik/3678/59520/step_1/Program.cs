59f2d15cec2f686c579a727fbf787dd89810565c
analyst <tshemake@gmail.com>
2018-01-08 00:27:21 +0500

    3678/59520/step_1

---
+++ stepik/3678/59520/step_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Переменная окружения в которой хранится имя файла-сокета?
+ */
+
+namespace step_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SSH_AUTH_SOCK");
+        }
+    }
+}
