f2e13e3eaab507640032e400a42318311b03c9f3
analyst <tshemake@gmail.com>
2018-01-07 21:27:53 +0500

    191/13187/step_4

---
+++ stepik/191/13187/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Кому можно сообщать свой платёжный пароль или код подтверждения
+ * по телефону?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("никому и ни при каких обстоятельствах.");
+        }
+    }
+}
