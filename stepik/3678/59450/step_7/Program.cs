6972159ab428dc42e49ef83ab75b5d93aef0eea9
analyst <tshemake@gmail.com>
2018-01-07 23:20:58 +0500

    3678/59450/step_7

---
+++ stepik/3678/59450/step_7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Проверьте корректность прав доступа к файлам ключей сервера
+ * sshd; файлы публичных ключей должны читаться любым пользователем,
+ * а файлы закрытых ключей - только пользователем root. Файлы
+ * ключей находятся  в папке /etc/ssh
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("for f in /etc/ssh/ssh_host_*_key; do sudo chmod 600 $f; done");
+            Console.WriteLine("for f in /etc/ssh/ssh_host_*_key.pub; do sudo chmod 644 $f; done");
+        }
+    }
+}
