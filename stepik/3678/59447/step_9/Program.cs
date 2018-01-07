5477d89448f6eb5d92ca2f1b2cf9f96515d835ad
analyst <tshemake@gmail.com>
2018-01-07 22:59:26 +0500

    3678/59447/step_9

---
+++ stepik/3678/59447/step_9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+ * Пользователя сильно раздражает необходимость вводить пароль при
+ * каждом SSH соединении. Поэтому необходимо авторизоваться с
+ * помощью public и private ключей.Используйте ssh-keygen и
+ * ssh-copy-id. Пользователь dima, пароль guba. Сервер localhost.
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh dima@localhost");
+            Console.WriteLine("mkdir .ssh");
+            Console.WriteLine("logout");
+            Console.WriteLine("ssh-copy-id dima@localhost");
+            Console.WriteLine("ssh dima@localhost");
+        }
+    }
+}
