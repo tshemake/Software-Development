b88eb6658f618ff0362c90f613e5f173867f4098
analyst <tshemake@gmail.com>
2018-01-07 22:59:23 +0500

    3678/59447/step_8

---
+++ stepik/3678/59447/step_8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Использование какой команды позволит копировать ключ на сервер?
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh-copy-id");
+        }
+    }
+}
