ee714548c5b2db07fd9d123a6dc7501af55dd307
analyst <tshemake@gmail.com>
2018-01-07 23:56:19 +0500

    3678/59501/step_4

---
+++ stepik/3678/59501/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Для чего используется ssh-agent?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("для хранения ключей");
+        }
+    }
+}
