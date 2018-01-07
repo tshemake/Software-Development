0bf461cff6af71a49ceeca6cf14f33ec6388bc21
analyst <tshemake@gmail.com>
2018-01-07 23:20:41 +0500

    3678/59450/step_2

---
+++ stepik/3678/59450/step_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какие права стоит устанавливать для закрытых ключей?
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("На чтение и запись только суперпользователю");
+        }
+    }
+}
