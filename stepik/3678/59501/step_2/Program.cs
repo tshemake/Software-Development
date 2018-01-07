bc7e165773874cf546336e8cea9496ba7934ca87
analyst <tshemake@gmail.com>
2018-01-07 23:56:10 +0500

    3678/59501/step_2

---
+++ stepik/3678/59501/step_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Что-такое ssh-agent?
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("специальная программа");
+        }
+    }
+}
