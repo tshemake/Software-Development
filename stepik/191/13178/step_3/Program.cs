e5e1e316b0627660b775afa6fbbcce0a9cb37467
analyst <tshemake@gmail.com>
2018-01-07 19:37:32 +0500

    191/13178/step_3

---
+++ stepik/191/13178/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какие программы называют эксплойтами?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("те, которые используют уязвимости в программном обеспечении с целью навредить компьютеру");
+        }
+    }
+}
