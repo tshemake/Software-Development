7ad30c3b15bdb4bd8757b8c815c52fbc8909eb9b
analyst <tshemake@gmail.com>
2018-01-07 13:55:38 +0500

    73/4767/step_11

---
+++ stepik/73/4767/step_11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Что произойдет, если в команде
+ * sed -n "/[a-z]*&sol;p" text.txt не указывать опцию -n?
+ */
+
+namespace step_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Что произойдет, если в команде sed -n \"/[a-z]*/p\" text.txt не указывать опцию -n?");
+        }
+    }
+}
