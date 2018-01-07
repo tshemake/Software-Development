296ce21a9ffc6ec2a7982cc1fadf183555a21795
analyst <tshemake@gmail.com>
2018-01-07 13:55:25 +0500

    73/4767/step_9

---
+++ stepik/73/4767/step_9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Предположим, что в файле  text.txt записаны строки, показанные
+ * среди вариантов ответа. Отметьте только те из них, которые выведет
+ * на экран команда  grep -E "[xklXKL]?[uU]buntu$" text.txt.
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lubuntu is better than Ubuntu");
+            Console.WriteLine("Mac OS X, Windows, Ubuntu");
+            Console.WriteLine("I prefer Kubuntu");
+        }
+    }
+}
