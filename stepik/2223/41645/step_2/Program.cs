4c1afa50521c4e7e810c69be1181f34591bc0c12
analyst <tshemake@gmail.com>
2018-01-08 01:02:26 +0500

    2223/41645/step_2

---
+++ stepik/2223/41645/step_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * В каком году язык JavaScript получил свое имя?
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1995);
+        }
+    }
+}
