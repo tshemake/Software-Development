967d7016ac9da5666921ced3d17877ea6c5005f8
analyst <tshemake@gmail.com>
2018-01-07 19:56:10 +0500

    191/13183/step_3

---
+++ stepik/191/13183/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Что такое фишинг?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("кража учётных данных с помощью фальшивых сайтов, маскирующихся под настоящие");
+        }
+    }
+}
