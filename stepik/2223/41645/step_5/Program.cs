64b93221061259ba99c0b12e2eeb8a5b19e37887
analyst <tshemake@gmail.com>
2018-01-08 01:02:37 +0500

    2223/41645/step_5

---
+++ stepik/2223/41645/step_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Сопоставьте определения, которые вы узнали на предыдущих шагах.
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DOM - это модель структурирующая HTML-тэги и позволяющая обращаться к ним по уникальному адресу");
+            Console.WriteLine("BOM - это модель структурирующая элементы браузера и позволяющая обращаться к ним по уникальному адресу");
+            Console.WriteLine("JQuery - Библиотека JavaScript, упрощающая работу по взаимодействию JavaScript и HTML");
+            Console.WriteLine("JavaScript - обладает системой автоматической сборки мусора");
+            Console.WriteLine("Язык Си - не обладает системой автоматической сборки мусора");
+        }
+    }
+}
