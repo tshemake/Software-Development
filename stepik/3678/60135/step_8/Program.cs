149f3bbbae29b1e110edee8f56a47396225022ee
analyst <tshemake@gmail.com>
2018-01-08 00:48:39 +0500

    3678/60135/step_8

---
+++ stepik/3678/60135/step_8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Что хранится в  ~/.ssh/config?
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the per-user configuration file.");
+        }
+    }
+}
