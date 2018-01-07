99cd3b601fa915357ad5e5de775a70796c370365
analyst <tshemake@gmail.com>
2018-01-07 22:59:11 +0500

    3678/59447/step_2

---
+++ stepik/3678/59447/step_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Какими способами может быть произведена аутентификация
+ * пользователя на удаленной машине по ssh?
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("пароль");
+            Console.WriteLine("ключ");
+        }
+    }
+}
