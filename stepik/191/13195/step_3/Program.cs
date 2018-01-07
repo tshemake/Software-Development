5901fab88ea094da12b3053f5a5e084103210afe
analyst <tshemake@gmail.com>
2018-01-07 21:30:11 +0500

    191/13195/step_3

---
+++ stepik/191/13195/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Как проверить, не мошеннический ли фонд?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("поискать упоминания о фонде в СМИ;");
+        }
+    }
+}
