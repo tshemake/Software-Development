c7a4d268029c4ce7d9c69991613283863d92ffa5
analyst <tshemake@gmail.com>
2018-01-07 23:29:24 +0500

    3678/59451/step_1

---
+++ stepik/3678/59451/step_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Можно ли закрыть ключ парольной фразой?
+ */
+
+namespace step_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("да");
+        }
+    }
+}
