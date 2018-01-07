96cf8a0e08d3e2c4febe654bde97d807871f7737
analyst <tshemake@gmail.com>
2018-01-07 21:30:40 +0500

    191/13196/step_4

---
+++ stepik/191/13196/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Как лучше всего сообщить близким номер своей банковской карты?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("лично;");
+        }
+    }
+}
