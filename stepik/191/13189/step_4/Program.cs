3747b281591f2f83846212bd1fd75d07188cb7a9
analyst <tshemake@gmail.com>
2018-01-07 21:28:33 +0500

    191/13189/step_4

---
+++ stepik/191/13189/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Как распознать поддельный сайт?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("по «опечаткам» в адресной строке (например, http://money-yanbex.ru)");
+        }
+    }
+}
