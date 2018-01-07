98417800dfc188cd3f7525ac5f380f7b7e9aeea6
analyst <tshemake@gmail.com>
2018-01-07 19:53:08 +0500

    191/13182/step_3

---
+++ stepik/191/13182/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Что такое двухфакторная аутентификация?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("пароль+одноразовый код");
+        }
+    }
+}
