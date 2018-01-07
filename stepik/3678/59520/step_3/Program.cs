6e97ce0799338d71f78e7bf35f99ec083e17742b
analyst <tshemake@gmail.com>
2018-01-08 00:27:27 +0500

    3678/59520/step_3

---
+++ stepik/3678/59520/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какие права рекомендуется устанавливать для закрытого ключа?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("600");
+        }
+    }
+}
