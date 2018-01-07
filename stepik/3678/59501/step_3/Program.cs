2f536e20962e5ee6203af827934fbbecca4a9053
analyst <tshemake@gmail.com>
2018-01-07 23:56:16 +0500

    3678/59501/step_3

---
+++ stepik/3678/59501/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Где хранится идентификатор процесса агента.
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SSH_AGENT_PID");
+        }
+    }
+}
