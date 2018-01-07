912e716dcfb7c2131e02fbc015dff84f5fd95921
analyst <tshemake@gmail.com>
2018-01-07 19:43:43 +0500

    191/13180/step_4

---
+++ stepik/191/13180/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Стоит ли обновлять браузеры?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("да, стоит, потому что в новых версиях исправляют уязвимости и недоработки предыдущих версий");
+        }
+    }
+}
