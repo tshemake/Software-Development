8e6c9bfe09ddad3a15dba6ae09d0b6616dc4d0e6
analyst <tshemake@gmail.com>
2018-01-07 19:37:12 +0500

    191/13177/step_4

---
+++ stepik/191/13177/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Кто может стать жертвой интернет-мошенников?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("любой пользователь интернета");
+        }
+    }
+}
