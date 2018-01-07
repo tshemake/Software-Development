e06f34088f65c4bd94978134ad44f14bfc260b0c
analyst <tshemake@gmail.com>
2018-01-07 21:29:01 +0500

    191/13190/step_4

---
+++ stepik/191/13190/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Вам пришло письмо от имени Яндекс.Денег. Оно не мошенническое,
+ * если пришло с адреса с доменным именем:
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("@money.yandex.ru");
+        }
+    }
+}
