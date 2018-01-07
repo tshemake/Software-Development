87cba3348ec61477c6ee6bd0d9d85e8a3b722824
analyst <tshemake@gmail.com>
2018-01-07 21:27:21 +0500

    191/13186/step_3

---
+++ stepik/191/13186/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Где в интернете можно столкнуться с мошенничеством?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("где угодно — от сообщения в соцсети до сайта известного магазина.");
+        }
+    }
+}
