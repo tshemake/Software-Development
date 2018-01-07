8bdadb6390d5d63750a6f838d18000d2e9896ac7
analyst <tshemake@gmail.com>
2018-01-08 00:48:32 +0500

    3678/60135/step_6

---
+++ stepik/3678/60135/step_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * OpenSSH SSH Клиент поддерживает некоторое количество протоколов
+ * аутентификации. Внимание вопрос, какое количество версий протоколов
+ * он поддерживает?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2);
+        }
+    }
+}
