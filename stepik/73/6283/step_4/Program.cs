bf89dca24e0d94864eb40026b74fa970675e2b33
analyst <tshemake@gmail.com>
2018-01-07 17:15:27 +0500

    73/6283/step_4

---
+++ stepik/73/6283/step_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+ * Какая команда(ы) установят файлу file.txt права доступа
+ * rwxrw-r--, если изначально у него были права r--r--r--. Укажите
+ * все верные варианты ответа!
+ *
+ * Примечание: запись вида команда1; команда2; команда3 означает,
+ * что в терминале последовательно выполнились все три команды
+ * (сначала команда1, затем команда2 и, наконец, команда3).
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chmod u+wx file.txt; chmod g+w file.txt");
+            Console.WriteLine("chmod a+wx file.txt; chmod o-wx file.txt; chmod g-x file.txt");
+        }
+    }
+}
