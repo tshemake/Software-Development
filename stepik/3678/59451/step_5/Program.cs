ab8f99effa087377354712d34963c964db2e6b6c
analyst <tshemake@gmail.com>
2018-01-07 23:29:38 +0500

    3678/59451/step_5

---
+++ stepik/3678/59451/step_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С использованием какой команды вы отправите ключ на сервер?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ssh-copy-id");
+        }
+    }
+}
