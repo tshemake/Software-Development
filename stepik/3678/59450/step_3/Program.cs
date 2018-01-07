89cbe884dda4a760d61713b89325eb1959f7faaf
analyst <tshemake@gmail.com>
2018-01-07 23:20:45 +0500

    3678/59450/step_3

---
+++ stepik/3678/59450/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какие права стоит устанавливать для открытых ключей?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("На запись - суперпользователю, на чтение - всем пользователям");
+        }
+    }
+}
