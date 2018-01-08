796fefdf76889219838758eff395ab38b76ba9c0
analyst <tshemake@gmail.com>
2018-01-08 14:17:59 +0500

    2223/43619/step_9

---
+++ stepik/2223/43619/step_9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * В этом тесте нужно отметить все правильные определения.
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Функцию можно определить и сразу вызвать в рамках одной команды");
+            Console.WriteLine("Команду return можно вовсе не использовать в функции");
+            Console.WriteLine("Функция, имеющая имя, может вызывать сама себя");
+        }
+    }
+}
