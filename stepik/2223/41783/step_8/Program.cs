f89906e5508c89932a5f04a2ef7068ef21046b70
analyst <tshemake@gmail.com>
2018-01-08 13:40:49 +0500

    2223/41783/step_8

---
+++ stepik/2223/41783/step_8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*
+ * В этом задании вам нужно вычислить сумму всех четных чисел,
+ * встречающихся в ряду от 1 до числа (включительно), передаваемого
+ * в нашу функцию (переменная "а").
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testWhile(3));
+        }
+
+        private static double testWhile(double a)
+        {
+            double x = 0;
+            while (a > 0)
+            {
+                if (a % 2 == 0)
+                {
+                    x += a;
+                }
+                a--;
+            }
+            return x;
+        }
+    }
+}
