6eb23370dd7f5520fbc63fb35394c49d7fd09cfe
analyst <tshemake@gmail.com>
2018-01-08 15:58:19 +0500

    2223/43621/step_9

---
+++ stepik/2223/43621/step_9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Как всегда в конце урока сначала небольшой тест!
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Метод pop() удаляет из массива один элемент - последний");
+            Console.WriteLine("Единственный метод, позволяющий добавлять элементы в середину массива - это метод splice()");
+        }
+    }
+}
