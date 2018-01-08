6c424c70ec3c5dfeedea5a22769c982fad4e58fa
analyst <tshemake@gmail.com>
2018-01-08 13:06:16 +0500

    2223/42024/step_11

---
+++ stepik/2223/42024/step_11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Давайте проверим, насколько понятно был описан материал
+ * предыдущего шага!
+ */
+
+namespace step_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("При x = 3 и y = 6 результатом операции x < 3 && y == 6 будет false");
+            Console.WriteLine("При x = 3 и y = 6 результатом операции !(x > y) будет true");
+            Console.WriteLine("При x = 3 и y = 6 результатом операции x == 3 || y > 6 будет true");
+        }
+    }
+}
