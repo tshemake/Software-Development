7e1d6f603879f7ff38f879387f2455bb41675bfd
analyst <tshemake@gmail.com>
2018-01-08 12:41:25 +0500

    2223/41646/step_2

---
+++ stepik/2223/41646/step_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+ * Теперь давайте проверим, насколько внимательно вы читали
+ * предыдущий шаг.
+ * Отметьте все допустимые варианты именования переменной.
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ExAmple");
+            Console.WriteLine("example");
+            Console.WriteLine("Example");
+            Console.WriteLine("$example");
+            Console.WriteLine("_example");
+            Console.WriteLine("EXAMPLE");
+        }
+    }
+}
