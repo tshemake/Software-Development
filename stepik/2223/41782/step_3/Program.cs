d24ebab88d40567410f8d6512b515a23a4bf8a79
analyst <tshemake@gmail.com>
2018-01-08 13:24:32 +0500

    2223/41782/step_3

---
+++ stepik/2223/41782/step_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+ * В этом задании вам нужно вписать в указанное место код,
+ * который будет присваивать переменной "х" значение суммы
+ * переменных "a" и "b" в случае если  a > b или их произведение
+ * в остальных случаях.
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testIf(3, 7));
+        }
+
+        private static double testIf(double a, double b)
+        {
+            double x;
+            x = (a > b) ? a + b : a * b;
+            return x;
+        }
+    }
+}
---
+++ stepik/2223/41782/step_3/script.js
@@ -0,0 +1,5 @@
+function testIf(a, b) {
+    var x;
+    x = (a > b) ? a + b : a * b;
+    return x;
+}
