e86824476bbad124457f525c655d3a38c58dea1e
analyst <tshemake@gmail.com>
2018-01-08 22:41:56 +0500

    2223/42159/step_5

---
+++ stepik/2223/42159/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Выберите правильные варианты
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("При обработке исключения должен присутствовать как минимум один блок - catch или finally");
+            Console.WriteLine("Исключение ловится командой catch");
+            Console.WriteLine("Бросить в качестве исключения можно практически любой тип данных");
+        }
+    }
+}
