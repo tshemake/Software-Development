73ad32043d5d866c8be0b79cb235f9e06f2ab77d
analyst <tshemake@gmail.com>
2018-01-08 18:50:00 +0500

    2223/43624/step_7

---
+++ stepik/2223/43624/step_7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+/*
+ * В этом задании в нашу функцию передаются 4 числа. Вам необходимо
+ * вычислить результат деления большего из этих чисел на меньшее, и
+ * округлив до ближайшего меньшего целого вернуть из функции.
+ *
+ * Sample Input:
+ *
+ *  1 4 8 8
+ * Sample Output:
+ *
+ *  8
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testMath(1, 4, 8, 8));
+        }
+
+        private static double testMath(double a, double b, double c, double d)
+        {
+            double[] array = { a, b, c, d };
+            double max = array.Max();
+            double min = array.Min();
+            return Math.Floor(max / min);
+        }
+    }
+}
---
+++ stepik/2223/43624/step_7/script.js
@@ -0,0 +1,5 @@
+function testMath(a, b, c, d) {
+    let max = Math.max(a, b, c, d);
+    let min = Math.min(a, b, c, d);
+    return Math.floor(max / min);
+}
