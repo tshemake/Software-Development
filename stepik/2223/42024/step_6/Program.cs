3f59d986646ce72796681fa688e0b085f05c7af0
analyst <tshemake@gmail.com>
2018-01-08 13:06:03 +0500

    2223/42024/step_6

---
+++ stepik/2223/42024/step_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Сопоставьте результаты вычисления операций присваивания при
+ * изначальных значениях х = 6 и y = 3;
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x += y; x = 9");
+            Console.WriteLine("x -= y; x = 3");
+            Console.WriteLine("x *= y; x = 18");
+            Console.WriteLine("x /= y; x = 2");
+            Console.WriteLine("x %= y; x = 0");
+        }
+    }
+}
