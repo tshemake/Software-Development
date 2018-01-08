58c864fbbce9da02d09a345aa4843ebc30cb9cad
analyst <tshemake@gmail.com>
2018-01-08 13:06:11 +0500

    2223/42024/step_9

---
+++ stepik/2223/42024/step_9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Отметьте все верные, с вашей точки зрения, ответы при значении
+ * x равном 7.
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Результатом x > 8 будет false");
+            Console.WriteLine("Результатом x == 8 будет false");
+            Console.WriteLine("Результатом x >= 7 будет true");
+            Console.WriteLine("Результатом x != 8 будет true");
+        }
+    }
+}
