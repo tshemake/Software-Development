7f2e556bb402b2d6ba3f11a700bf3ff58bef80f5
analyst <tshemake@gmail.com>
2018-01-08 13:06:19 +0500

    2223/42024/step_12

---
+++ stepik/2223/42024/step_12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+ * В этом задании вам нужно вписать в указанное место код,
+ * который будет присваивать переменной "х" удвоенное значение
+ * остатка от деления произведения переменных "a" и "b" на их сумму.
+ */
+
+namespace step_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testOperation(6, 4));
+        }
+
+        private static double testOperation(double a, double b)
+        {
+            double x;
+            x = (a * b % (a + b)) * 2;
+            return x;
+        }
+    }
+}
