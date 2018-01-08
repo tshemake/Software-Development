f0d72fbe705bc1dfdd1385cd6b8bcae5e758038a
analyst <tshemake@gmail.com>
2018-01-08 14:32:00 +0500

    2223/43727/step_11

---
+++ stepik/2223/43727/step_11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Теперь проверим, насколько понятно был изложен предыдущий материал!
+ */
+
+namespace step_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ключевое слово this используется для доступа к объекту, из которого оно вызывается");
+            Console.WriteLine("Выполнение кода function Person(name, age, year) { this.name = name;} создаст конструктор для объекта person");
+            Console.WriteLine("Если присвоить значению какого-либо свойства код, то мы получим метод");
+        }
+    }
+}
