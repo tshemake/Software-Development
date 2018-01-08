4dd3fe60a8d8255af173798eb08de0bc5ebaf90b
analyst <tshemake@gmail.com>
2018-01-08 12:41:31 +0500

    2223/41646/step_5

---
+++ stepik/2223/41646/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * В этом тесте вам нужно выбрать все правильные варианты
+ * объявления и инициализации переменных.
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("var x = 12;");
+            Console.WriteLine("x = 22;");
+        }
+    }
+}
