95c668d71850eba243dc1959bd0104f89ccc47ac
analyst <tshemake@gmail.com>
2018-01-08 23:46:50 +0500

    2223/51493/step_5

---
+++ stepik/2223/51493/step_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Теперь посмотрим, насколько понятно был изложен материал по
+ * методам объекта  String, работающим с регулярными выражениями.
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Метод replace(regex, str) поддерживает глобальный поиск");
+            Console.WriteLine("Результатом работы метода split() является массив");
+            Console.WriteLine("Если в методы replace и search передать вместо регулярного выражения строку, то она будет преобразована в регулярное выражение");
+        }
+    }
+}
