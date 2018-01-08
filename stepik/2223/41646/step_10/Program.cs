1d338b691f363f856376a389cb26629a2b920a0f
analyst <tshemake@gmail.com>
2018-01-08 12:41:40 +0500

    2223/41646/step_10

---
+++ stepik/2223/41646/step_10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+ * В этом тесте вам нужно сопоставить объявления переменных и их тип.
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("var x; переменная х имеет тип undefined");
+            Console.WriteLine("var x = 5; переменная х имеет тип number");
+            Console.WriteLine("var y = '5'; переменная y имеет тип string");
+            Console.WriteLine("var z = \"5\"; переменная z имеет тип string");
+            Console.WriteLine("var x = true; переменная х имеет тип boolean");
+            Console.WriteLine("var x = 'true'; переменная х имеет тип string");
+            Console.WriteLine("var y = 0; переменная y имеет тип number");
+            Console.WriteLine("var y = false; переменная y имеет тип boolean");
+            Console.WriteLine("var z = 5; переменная z имеет тип number");
+        }
+    }
+}
