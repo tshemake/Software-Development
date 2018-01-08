94ac594373a886de9e4592c3d8f88ca4618fa000
analyst <tshemake@gmail.com>
2018-01-08 22:59:35 +0500

    2223/42160/step_4

---
+++ stepik/2223/42160/step_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Задача на разминку.
+ * Укажите регулярные выражения, которые будут соответствовать
+ * какой либо подстроке в строке "http://www.stepik.org";
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/www/");
+            Console.WriteLine("/\\//");
+            Console.WriteLine("/\\/\\//");
+        }
+    }
+}
