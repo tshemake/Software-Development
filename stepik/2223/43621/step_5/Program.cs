46dd9adc42afca10cd6358a4f0810b176f44f2f4
analyst <tshemake@gmail.com>
2018-01-08 15:58:12 +0500

    2223/43621/step_5

---
+++ stepik/2223/43621/step_5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/*
+ * В этом задании в нашу функцию testArray передаются два массива
+ * случайной длины заполненные случайными числами. Вам нужно сосчитать
+ * сумму всех элементов обоих массивов и возвратить ее из функции.
+ *
+ * Sample Input:
+ *
+ *  [8, 1, 1, 7, 4, 0]  [5, 8, 5, 4, 8]
+ * Sample Output:
+ *
+ *  51
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testArray(new double[] { 8, 1, 1, 7, 4, 0 },
+                                        new double[] { 5, 8, 5, 4, 8 }));
+        }
+
+        private static double testArray(double[] a, double[] b)
+        {
+            double sum = 0;
+            foreach (double i in a)
+            {
+                sum += i;
+            }
+            foreach (double i in b)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
