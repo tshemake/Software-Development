3486e69fc1ac3c591212116a65c339ba171f8d12
analyst <tshemake@gmail.com>
2018-01-08 23:49:53 +0500

    2223/51493/step_10

---
+++ stepik/2223/51493/step_10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Давайте проверим, что мы усвоили про объект RegExp.
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Объект RegExp имеет пять свойств");
+            Console.WriteLine("Конструктор объекта RegExp может принимать на вход либо один либо два аргумента");
+        }
+    }
+}
