e826d34139097e53a29fd4efdd2aa42faa2d674c
analyst <tshemake@gmail.com>
2018-01-08 22:41:51 +0500

    2223/42159/step_2

---
+++ stepik/2223/42159/step_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Как вы считаете, исключение можно : ...
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Все перечисленные варианты");
+        }
+    }
+}
