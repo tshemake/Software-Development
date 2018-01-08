1d6313aed87dff84363cfa41c117c2d6d7c03a03
analyst <tshemake@gmail.com>
2018-01-08 18:49:52 +0500

    2223/43624/step_4

---
+++ stepik/2223/43624/step_4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+ * В этом задании в нашу функцию передается угол в градусах.
+ * Вам нужно вернуть из функции значение его синуса. Не забывайте,
+ * что тригонометрические функции в JavaScript принимают аргументы
+ * только в радианах!
+ *
+ * Sample Input:
+ *
+ * 348
+ * Sample Output:
+ *
+ * -0.20791169081775987
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testMath(348));
+        }
+
+        private static double testMath(int a)
+        {
+            return Math.Sin((Math.PI / 180) * a);
+        }
+    }
+}
