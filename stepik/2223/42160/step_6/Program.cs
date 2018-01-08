833ad4f6c5b74aa87a616dfe4d750479564b72b7
analyst <tshemake@gmail.com>
2018-01-08 22:59:40 +0500

    2223/42160/step_6

---
+++ stepik/2223/42160/step_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Отметьте выражения, которые будут соответствовать строкам или
+ * подстрокам.
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/[^qwerty]/ для строки \"123qwerty456\"");
+            Console.WriteLine("/\\d/ для строки \"123qwerty456\"");
+            Console.WriteLine("/qwerty/ для строки \"123qwerty456\"");
+            Console.WriteLine("/qwerty/ для строки \"qwerty\"");
+            Console.WriteLine("/\\w/ для строки \"qwerty\"");
+        }
+    }
+}
