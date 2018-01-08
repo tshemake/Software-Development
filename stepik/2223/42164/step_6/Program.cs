c261dcf0c2adf0c1c05f9e0b85631ac798eb0dd4
analyst <tshemake@gmail.com>
2018-01-08 23:50:37 +0500

    2223/42164/step_6

---
+++ stepik/2223/42164/step_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+ * А теперь - задача на... программирование!) 
+ * В тексте внизу описана анонимная функция, внутри которой
+ * находится замыкание, изменяющее значение переменной во внешней
+ * функции и возвращающее ее. Но вот проблема - строчки перепутались
+ * местами! Восстановите необходимую последовательность строк.
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("var enumerator = (function () {");
+            Console.WriteLine("var actual = 0;");
+            Console.WriteLine("return function () {");
+            Console.WriteLine("actual++;");
+            Console.WriteLine("return actual;");
+            Console.WriteLine("}");
+            Console.WriteLine("})();");
+        }
+    }
+}
