ae6994ccb30f9dd0306cc3e6df0a95792a44ab0e
analyst <tshemake@gmail.com>
2018-01-08 14:17:48 +0500

    2223/43619/step_5

---
+++ stepik/2223/43619/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Отметьте все варианты корректного определения функции
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("function myFunction() { };");
+            Console.WriteLine("function Function(a) { document.write(a); };");
+            Console.WriteLine("var myFunction = function(a) {document.write(a); } ;");
+        }
+    }
+}
