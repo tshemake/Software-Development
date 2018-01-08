b1d8df12e918a20567aa79b5526035113624dd7c
analyst <tshemake@gmail.com>
2018-01-08 18:22:18 +0500

    2223/43620/step_7

---
+++ stepik/2223/43620/step_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Перед финальным заданием этого урока ответим на несколько
+ * несложных вопросов для проверки понятности материала.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Метод getDay() возвращает день недели даты");
+            Console.WriteLine("После объявления переменной var myDate = new Date(); в переменной окажется текущая дата и время");
+            Console.WriteLine("Объекты типа Date можно сравнивать между собой");
+        }
+    }
+}
