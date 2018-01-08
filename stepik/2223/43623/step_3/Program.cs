9b8bb373f95219caac5da7ff6fbeef1e415bbffe
analyst <tshemake@gmail.com>
2018-01-08 15:14:10 +0500

    2223/43623/step_3

---
+++ stepik/2223/43623/step_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*
+ * В этом задании в нашу функцию testStr передаются две строки.
+ * Вам нужно вернуть из функции их суммарную длину.
+ *
+ * Sample Input:
+ *
+ * 5794675 181683
+ * Sample Output:
+ *
+ * 13
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testStr("5794675", "181683"));
+        }
+
+        private static double testStr(string a, string b)
+        {
+            return a.Length + b.Length;
+        }
+    }
+}
