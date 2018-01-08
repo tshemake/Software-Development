37afd18375edaa8703157b02d2c969baa25371bf
analyst <tshemake@gmail.com>
2018-01-08 15:14:23 +0500

    2223/43623/step_12

---
+++ stepik/2223/43623/step_12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+ * В этом задании в нашу функцию testStr передаются две строки.
+ * Вам нужно вернуть индекс позиции, с которой начинается вхождение
+ * второй строки в первую.
+ *
+ * Sample Input:
+ *
+ *  This is a test string
+ *  test
+ * Sample Output:
+ *
+ *  10
+ */
+
+namespace step_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testStr("This is a test string", "test"));
+        }
+
+        private static int testStr(string a, string b)
+        {
+            return a.IndexOf(b);
+        }
+    }
+}
