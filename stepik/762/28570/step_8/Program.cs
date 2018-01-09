2730d373f4fa8b18a0bf9f2c64c7d7b757a822d1
analyst <tshemake@gmail.com>
2018-01-09 21:02:58 +0500

    762/28570/step_8

---
+++ stepik/762/28570/step_8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Напишите строку для полного (со всеми файлами и поддиректориями)
+ * удаления директории /home/box
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("rm -rf /home/box");
+        }
+    }
+}
