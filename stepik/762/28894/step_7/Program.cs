b44b2b689e376287ebea8c973e33997c3d10a152
analyst <tshemake@gmail.com>
2018-01-09 21:16:43 +0500

    762/28894/step_7

---
+++ stepik/762/28894/step_7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * 1. С помощью утилиты cat создайте файл ~/resolv.conf из файлов
+ * head base google1 google2, расположенных в каталоге
+ * /etc/resolvconf/resolv.conf.d. Внимание! Сохраняйте
+ * последовательность файлов в команде cat, как указано в этом
+ * задании.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cat /etc/resolvconf/resolv.conf.d/{head,base,google1,google2} > resolv.conf");
+        }
+    }
+}
