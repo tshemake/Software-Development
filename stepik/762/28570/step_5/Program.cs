d570d2c5c9e6f96d41a6e26e4ea919f39f392d8c
analyst <tshemake@gmail.com>
2018-01-09 21:02:49 +0500

    762/28570/step_5

---
+++ stepik/762/28570/step_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Утилита rm удаляет файлы
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Навсегда");
+        }
+    }
+}
