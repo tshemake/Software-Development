8ce8d48863a0cf150a33165641d0592ce7388d96
analyst <tshemake@gmail.com>
2018-01-09 21:02:52 +0500

    762/28570/step_6

---
+++ stepik/762/28570/step_6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Имена файлов в Linux являются
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Регистрозависимыми");
+        }
+    }
+}
