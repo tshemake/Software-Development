7b591d6afb8bb45fabee5e2e85fe90cb1e49ce0e
analyst <tshemake@gmail.com>
2018-01-09 20:34:16 +0500

    762/27881/step_8

---
+++ stepik/762/27881/step_8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Напишите команду для создания структуры директорий
+ * /home/box/foo/bar/baz, если вы находитесь в домашней
+ * директории (/home/box).
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mkdir -p foo/bar/baz");
+        }
+    }
+}
