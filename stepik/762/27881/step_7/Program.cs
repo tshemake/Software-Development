1b9bdd4ef50046f966c4bb66828605f0f174b68f
analyst <tshemake@gmail.com>
2018-01-09 20:34:11 +0500

    762/27881/step_7

---
+++ stepik/762/27881/step_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какой ключ нужно передать команде mkdir, чтобы создать
+ * несколько вложенных каталогов одной командой?
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-p");
+        }
+    }
+}
