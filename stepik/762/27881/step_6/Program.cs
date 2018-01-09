86186ef097863fddb3d1d2d8e5693be9a3550bbf
analyst <tshemake@gmail.com>
2018-01-09 20:34:07 +0500

    762/27881/step_6

---
+++ stepik/762/27881/step_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какой параметр утилиты ls предназначен для вывода информации
+ * обо всех файлах, включая скрытые файлы?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-a");
+        }
+    }
+}
