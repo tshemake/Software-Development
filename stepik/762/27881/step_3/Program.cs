aa0b765e8a7e38537bd82bd110932c4472ab0354
analyst <tshemake@gmail.com>
2018-01-09 20:33:56 +0500

    762/27881/step_3

---
+++ stepik/762/27881/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * С помощью какой команды может быть получена информация о вашем
+ * текущем местонахождении в рамках файловой системы?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pwd");
+        }
+    }
+}
