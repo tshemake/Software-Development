c0644a837e20912dee8a87406fda9d81baaa9117
analyst <tshemake@gmail.com>
2018-01-09 21:02:44 +0500

    762/28570/step_4

---
+++ stepik/762/28570/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С помощью какой утилиты можно создать пустой файл?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("touch");
+        }
+    }
+}
