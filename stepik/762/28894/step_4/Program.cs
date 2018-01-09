3125e98c459f8e1ce5400c3b0d6276913d1cf9e9
analyst <tshemake@gmail.com>
2018-01-09 21:16:33 +0500

    762/28894/step_4

---
+++ stepik/762/28894/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какая утилита позволяет вывести несколько строк с конца файла?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tail");
+        }
+    }
+}
