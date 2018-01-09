a4776a6e0ffcb38fbd287eb46a8e97e7f1c5576f
analyst <tshemake@gmail.com>
2018-01-09 21:16:28 +0500

    762/28894/step_3

---
+++ stepik/762/28894/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какая утилита позволяет вывести несколько строк с начала файла?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("head");
+        }
+    }
+}
