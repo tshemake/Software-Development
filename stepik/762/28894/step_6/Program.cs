05a1db1704528abad6948b8283a5038bf30ccead
analyst <tshemake@gmail.com>
2018-01-09 21:16:39 +0500

    762/28894/step_6

---
+++ stepik/762/28894/step_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Напишите команду для вывода двух последних строк из файла
+ * /etc/fstab
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tail -2 /etc/fstab");
+        }
+    }
+}
