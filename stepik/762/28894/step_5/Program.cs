c42c841609900d81fce4fae7e107b6e190f5fbda
analyst <tshemake@gmail.com>
2018-01-09 21:16:36 +0500

    762/28894/step_5

---
+++ stepik/762/28894/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Какие утилиты позволяют просматривать длинные файлы, разделив
+ * их на страницы?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("more");
+            Console.WriteLine("less");
+        }
+    }
+}
