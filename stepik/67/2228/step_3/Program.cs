fcea4ad8f80180e18d33c4199122b9864c25b33e
analyst <tshemake@gmail.com>
2018-01-09 22:27:36 +0500

    67/2228/step_3

---
+++ stepik/67/2228/step_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(11111L * 1111111);
+        }
+    }
+}
