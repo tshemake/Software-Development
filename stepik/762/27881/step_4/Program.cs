f9116a2f0d1114fdf730f5b7cdb789625346039e
analyst <tshemake@gmail.com>
2018-01-09 20:34:00 +0500

    762/27881/step_4

---
+++ stepik/762/27881/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какой символ используется в начале пути файловой системы для
+ * указания абсолюного пути?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/");
+        }
+    }
+}
