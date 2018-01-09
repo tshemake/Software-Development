955320955fc73cb7c9939f949e501bed04924621
analyst <tshemake@gmail.com>
2018-01-09 20:34:02 +0500

    762/27881/step_5

---
+++ stepik/762/27881/step_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какая утилита предназначена для вывода списка содержимого
+ * директории?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ls");
+        }
+    }
+}
