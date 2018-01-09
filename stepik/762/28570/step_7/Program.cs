c2ab54d17e0abf3b3e19ee1c21f0ec67be816739
analyst <tshemake@gmail.com>
2018-01-09 21:02:55 +0500

    762/28570/step_7

---
+++ stepik/762/28570/step_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Для рекурсивного копирования файлов команде cp необходимо
+ * указать ключ
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-r");
+        }
+    }
+}
