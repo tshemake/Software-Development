ad6d6e911fa8b8226989da6089d8622a9ff63fcb
analyst <tshemake@gmail.com>
2018-01-09 21:02:41 +0500

    762/28570/step_3

---
+++ stepik/762/28570/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какая утилита предназначена для определения типа файлов?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("file");
+        }
+    }
+}
