236bd466c757d3e65d6ef3a5aa63ab3804d79705
analyst <tshemake@gmail.com>
2018-01-09 21:03:01 +0500

    762/28570/step_9

---
+++ stepik/762/28570/step_9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ * 1. Создайте пустые файлы file1 и file2 в домашней директории.
+ *
+ * 2. Создайте директорию ~/default, и скопируйте в нее все файлы
+ * из /etc/default.
+ *
+ * 3. Создайте директорию ~/newdir, и переместите в нее файлы
+ * rsyslog и syslog-ng из ~/default.
+ *
+ * 4. Удалите директорию /var/cache/apt со всем поддиректориями и
+ * файлами (используйте команду sudo для удаления с правами суперпользователя).
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("touch file1 file2");
+            Console.WriteLine("mkdir default");
+            Console.WriteLine("cp -R /etc/default/. default");
+            Console.WriteLine("mkdir newdir");
+            Console.WriteLine("mv default/{rsyslog,syslog-ng} newdir");
+            Console.WriteLine("sudo rm -rf /var/cache/apt");
+        }
+    }
+}
