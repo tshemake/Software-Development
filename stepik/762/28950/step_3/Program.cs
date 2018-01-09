ef591ff1f9f04a20c731137297702d0bb202e570
analyst <tshemake@gmail.com>
2018-01-09 20:20:43 +0500

    762/28950/step_3

---
+++ stepik/762/28950/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какая команда предназначена для чтения страниц руководств?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("man");
+        }
+    }
+}
