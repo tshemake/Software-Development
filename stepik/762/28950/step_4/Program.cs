e181958162ed9b490b0b1cc9cc8f6fb53d7232c2
analyst <tshemake@gmail.com>
2018-01-09 20:20:47 +0500

    762/28950/step_4

---
+++ stepik/762/28950/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Напишите команду, которую необходимо ввести, чтобы получить
+ * справку по утилите ls.
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("man ls");
+        }
+    }
+}
