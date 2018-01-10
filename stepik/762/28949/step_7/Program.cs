b8138df2cc22f8b0cf235ee3ac977637820da533
analyst <tshemake@gmail.com>
2018-01-10 22:51:17 +0500

    762/28949/step_7

---
+++ stepik/762/28949/step_7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * В какой директории хранятся файлы устройства?
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/dev");
+        }
+    }
+}
