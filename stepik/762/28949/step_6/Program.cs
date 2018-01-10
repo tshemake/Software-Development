807fbd0cfeeb96704eb6eb735fe8cf230b2228a8
analyst <tshemake@gmail.com>
2018-01-10 22:51:11 +0500

    762/28949/step_6

---
+++ stepik/762/28949/step_6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какая директория предназначена для хранения файлов конфигурации?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/etc");
+        }
+    }
+}
