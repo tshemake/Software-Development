9fa48dee3fe3dbad2669558865a4ee3c9fb405e0
analyst <tshemake@gmail.com>
2018-01-10 22:51:00 +0500

    762/28949/step_3

---
+++ stepik/762/28949/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * В поддиректориях какой директории пользователи могут хранить
+ * свои персональные данные?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/home");
+        }
+    }
+}
