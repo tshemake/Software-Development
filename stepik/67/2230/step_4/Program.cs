16457994c276dfc7557a36e460075ba4df017d0f
analyst <tshemake@gmail.com>
2018-01-10 23:52:47 +0500

    67/2230/step_4

---
+++ stepik/67/2230/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Запишите число 1.2345e-3 в виде десятичной дроби
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1.2345e-3);
+        }
+    }
+}
