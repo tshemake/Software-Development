259931769277166d3a74a81afcca9f1c5cd49ce0
analyst <tshemake@gmail.com>
2018-01-10 23:42:19 +0500

    762/28979/step_11

---
+++ stepik/762/28979/step_11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Сопоставьте бинарные файлы с их директориями
+ */
+
+namespace step_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("alias - ");
+            Console.WriteLine("route - /sbin");
+            Console.WriteLine("rm - /bin");
+            Console.WriteLine("tac - /usr/bin");
+        }
+    }
+}
