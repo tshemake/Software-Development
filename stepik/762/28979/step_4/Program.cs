443ffe5ffc5beb9a5efde134907ed2c4ba96a474
analyst <tshemake@gmail.com>
2018-01-10 23:41:49 +0500

    762/28979/step_4

---
+++ stepik/762/28979/step_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * В каких случаях количество пробелов между словами foo и bar
+ * будет сохранено в результате выполнения команды echo?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("echo 'foo bar'");
+            Console.WriteLine("echo \"foo bar\"");
+        }
+    }
+}
