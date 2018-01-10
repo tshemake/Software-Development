4eb6dbd1e04e1cf139414a04790bb4c1080a5d52
analyst <tshemake@gmail.com>
2018-01-11 00:08:42 +0500

    67/2231/step_4

---
+++ stepik/67/2231/step_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+/*
+ * Вычислите в python значение выражения
+ * 9**19 - int(float(9**19))
+ *
+ * Заметьте, что используется одно и то же число, но результат
+ * получается не нулевой. Это показывает различие хранения
+ * большого числа в целочисленном типе и типе с плавающей точкой.
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Pow(9, 19) - Math.Pow(9, 19));
+            Console.WriteLine(BigInteger.Pow(9, 19) - BigInteger.Pow(9, 19));
+            Console.WriteLine(89);
+        }
+    }
+}
