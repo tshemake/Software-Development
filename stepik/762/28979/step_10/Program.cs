04d7d166bd61462caa9fd34a130ba94bbbc4cb36
analyst <tshemake@gmail.com>
2018-01-10 23:42:12 +0500

    762/28979/step_10

---
+++ stepik/762/28979/step_10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Отметьте внешние команды
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("echo");
+            Console.WriteLine("which");
+            Console.WriteLine("rm");
+            Console.WriteLine("route");
+            Console.WriteLine("tac");
+        }
+    }
+}
