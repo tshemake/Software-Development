17736e67922b1aca3a67cd1e7d8d824238a3700d
analyst <tshemake@gmail.com>
2018-01-10 23:41:57 +0500

    762/28979/step_7

---
+++ stepik/762/28979/step_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Напишите команду для создания псевдонима для команды "ls -lh",
+ * и назовите псевдоним "lh".
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("alias lh=\"ls -lh\"");
+        }
+    }
+}
