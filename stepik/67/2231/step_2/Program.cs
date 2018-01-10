f5a5df6c23ea007b65e89fd214018cea7bd599cb
analyst <tshemake@gmail.com>
2018-01-11 00:08:36 +0500

    67/2231/step_2

---
+++ stepik/67/2231/step_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Составьте выражение для вычисления в интерпретаторе Python 3 и
+ * вставьте в поле ответа результат вычисления.
+ *
+ * Приведите к целому типу число 2.99
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)2.99);
+        }
+    }
+}
