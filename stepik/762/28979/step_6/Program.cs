d1ab7445dcf8aa1f8c95015a9c74343c44f3cc70
analyst <tshemake@gmail.com>
2018-01-10 23:41:54 +0500

    762/28979/step_6

---
+++ stepik/762/28979/step_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какая команда осуществляет поиск бинарных файлов в директории,
+ * заданной с помощью переменной окружения $PATH?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("which");
+        }
+    }
+}
