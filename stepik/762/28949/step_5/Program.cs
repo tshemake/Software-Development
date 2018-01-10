4887accbda50eb60541ae0ace380fea4f42d4e96
analyst <tshemake@gmail.com>
2018-01-10 22:51:07 +0500

    762/28949/step_5

---
+++ stepik/762/28949/step_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какая директория служит точкой монтирования для съемных устройств?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/media");
+        }
+    }
+}
