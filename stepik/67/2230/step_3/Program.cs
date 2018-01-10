005ba5e5932184c5e1a81a8c39a3bb6927bb6560
analyst <tshemake@gmail.com>
2018-01-10 23:52:42 +0500

    67/2230/step_3

---
+++ stepik/67/2230/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Запишите число 1.2345e3 в виде десятичной дроби.
+ * См. экспоненциальная запись.
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1.2345e3);
+        }
+    }
+}
