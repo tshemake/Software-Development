515bbbf63a20e64f24d9a88d62fcbd568f23b598
analyst <tshemake@gmail.com>
2018-01-10 23:41:46 +0500

    762/28979/step_3

---
+++ stepik/762/28979/step_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Сколько аргументов передается с помощью данной строки команды
+ * (не считая самой команды)
+ * touch '/etc/cron/cron.allow' 'file 42.txt' "file 33.txt" file11.txt
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(4);
+        }
+    }
+}
