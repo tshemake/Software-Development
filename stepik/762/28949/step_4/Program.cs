cb4dc4e69b9e106705ab0e6208f4f48ec8235639
analyst <tshemake@gmail.com>
2018-01-10 22:51:04 +0500

    762/28949/step_4

---
+++ stepik/762/28949/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С какого символа начинаются имена скрытых файлов и директорий?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("точки .");
+        }
+    }
+}
