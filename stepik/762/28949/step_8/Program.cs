5408495eb022a64ce6c21f2be3a3c06d2a19829a
analyst <tshemake@gmail.com>
2018-01-10 22:51:20 +0500

    762/28949/step_8

---
+++ stepik/762/28949/step_8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какая директория является центральным хранилищем всех журналов,
+ * в т.ч. журналов ОС?
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/var/log");
+        }
+    }
+}
