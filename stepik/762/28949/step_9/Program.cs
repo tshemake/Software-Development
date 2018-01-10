d11e04d788185ad546c84e5b176e2b53bdf231cb
analyst <tshemake@gmail.com>
2018-01-10 22:51:24 +0500

    762/28949/step_9

---
+++ stepik/762/28949/step_9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * В какой директории могут содержаться кэшированные данные
+ * некоторых приложений?
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/var/cache");
+        }
+    }
+}
