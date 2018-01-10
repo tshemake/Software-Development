87b7489acb0d018e48b826dd60766e2c04191e80
analyst <tshemake@gmail.com>
2018-01-10 23:42:03 +0500

    762/28979/step_9

---
+++ stepik/762/28979/step_9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Отметьте команды, встроенные в командную оболочку
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("echo");
+            Console.WriteLine("alias");
+        }
+    }
+}
