47f8e7d775b7905309d4e9c74932e2887e82a5b5
analyst <tshemake@gmail.com>
2018-01-10 23:56:02 +0500

    67/2230/step_6

---
+++ stepik/67/2230/step_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Составьте выражение для вычисления в интерпретаторе Python 3 и
+ * вставьте в поле ответа результат вычисления:
+ *
+ * 7/3
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(7d / 3);
+        }
+    }
+}
