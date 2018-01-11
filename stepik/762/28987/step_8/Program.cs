0d5c503db7661c906e5edd937d976deaccf3a6b3
analyst <tshemake@gmail.com>
2018-01-11 21:24:17 +0500

    762/28987/step_8

---
+++ stepik/762/28987/step_8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какую команду необходимо использовать для удаления переменной
+ * окружения?
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("unset");
+        }
+    }
+}
