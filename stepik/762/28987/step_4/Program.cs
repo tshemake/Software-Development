4660d3dd43ec639b61af35115c2ab7a16104a13a
analyst <tshemake@gmail.com>
2018-01-11 21:24:17 +0500

    762/28987/step_4

---
+++ stepik/762/28987/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Присвойте переменной TestVar значение 123.
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TestVar=123");
+        }
+    }
+}
