653708e12cb06212af6a415544ebcb9823d713e1
analyst <tshemake@gmail.com>
2018-01-11 21:12:34 +0500

    762/28981/step_5

---
+++ stepik/762/28981/step_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Сопоставьте логические операторы с операциями
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("| - ");
+            Console.WriteLine("|| - логическое \"ИЛИ\"");
+            Console.WriteLine("& - ");
+            Console.WriteLine("&& - логическое \"И\"");
+        }
+    }
+}
