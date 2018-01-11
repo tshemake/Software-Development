5083570224cd1e60d09b53dcb50fe37ab5ca5f7a
analyst <tshemake@gmail.com>
2018-01-11 21:24:18 +0500

    762/28987/step_10

---
+++ stepik/762/28987/step_10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+ * 1. Создайте переменную $MyVar, и присвойте ей значение
+ * 'Hello world!'. Экспортируйте $MyVar в дочерние командные оболочки.
+ *
+ * 2. Уничтожьте переменную $TestVar.
+ *
+ * 3. Выполните экспорт переменных окружения в файл ~/environment
+ * перед проверкой результата:
+ *
+ * env > ~/environment
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("export MyVar=\"Hello world!\"");
+            Console.WriteLine("unset TestVar");
+            Console.WriteLine("env > ~/environment");
+        }
+    }
+}
