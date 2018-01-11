15e2ebb689b8d0e572b9c550bb9bc4aeee62b4a6
analyst <tshemake@gmail.com>
2018-01-11 23:15:07 +0500

    2621/45424/step_3

---
+++ stepik/2621/45424/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * С помощью какого атрибута можно придать нужное оформление
+ * любому тегу?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("style");
+        }
+    }
+}
