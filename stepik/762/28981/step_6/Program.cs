9d8b158a0ed72431ca7af080ccaa7b285d631538
analyst <tshemake@gmail.com>
2018-01-11 21:12:35 +0500

    762/28981/step_6

---
+++ stepik/762/28981/step_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какой символ используется для написания комментариев в
+ * скриптах командной оболочки?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#");
+        }
+    }
+}
