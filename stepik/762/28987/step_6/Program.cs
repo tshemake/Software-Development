8fa3bcb36375eb5ddc076291ccff27ff5a04e205
analyst <tshemake@gmail.com>
2018-01-11 21:24:17 +0500

    762/28987/step_6

---
+++ stepik/762/28987/step_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * В качестве ответа запишите результат последовательного выполнения
+ * следующих команд:
+ *
+ * TestVar="Hello world!"
+ *
+ * echo '$TestVar'
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("$TestVar");
+        }
+    }
+}
