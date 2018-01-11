7ca8b875b61b213a8e42d1ac9fb63079d657c786
analyst <tshemake@gmail.com>
2018-01-11 23:12:05 +0500

    2621/45000/step_3

---
+++ stepik/2621/45000/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Тег для вставки изображения:
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<img src=\"...\">");
+        }
+    }
+}
