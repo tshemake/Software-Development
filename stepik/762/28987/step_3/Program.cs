e9a8d67c3a1ad23ed5af76a7f8662d534b673696
analyst <tshemake@gmail.com>
2018-01-11 21:24:17 +0500

    762/28987/step_3

---
+++ stepik/762/28987/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какой символ используется для обозначения переменных в
+ * командной оболочке?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("$");
+        }
+    }
+}
