84870dc27c739b438b0f2755f37548f778b55232
analyst <tshemake@gmail.com>
2018-01-11 22:13:04 +0500

    762/29561/step_5

---
+++ stepik/762/29561/step_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Сделайте так, чтобы ваша командная оболочка bash сохраняла в
+ * памяти 5000 введенных команд.
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HISTSIZE=5000");
+        }
+    }
+}
