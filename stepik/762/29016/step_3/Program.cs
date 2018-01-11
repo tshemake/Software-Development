8c4b1706457703df7c8d7efdf592b19a43f38bc0
analyst <tshemake@gmail.com>
2018-01-11 22:12:30 +0500

    762/29016/step_3

---
+++ stepik/762/29016/step_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * С помощью каких символов осуществляется встраивание командных
+ * оболочек?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("`...`");
+            Console.WriteLine("$(...)");
+        }
+    }
+}
