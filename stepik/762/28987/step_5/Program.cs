471181eeff2d5567e62e7e5424e284e42e58402c
analyst <tshemake@gmail.com>
2018-01-11 21:24:17 +0500

    762/28987/step_5

---
+++ stepik/762/28987/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * С помощью каких команд можно вывести список переменных
+ * командной оболочки?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("set");
+            Console.WriteLine("env");
+        }
+    }
+}
