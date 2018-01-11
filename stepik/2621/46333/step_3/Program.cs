8b4d4ccae95c08a27cb7871f51d0179b8fb0cf1c
analyst <tshemake@gmail.com>
2018-01-11 23:25:17 +0500

    2621/46333/step_3

---
+++ stepik/2621/46333/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Изображения, которые можно использовать в качестве фона
+ * web-страницы?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Бесшовные");
+        }
+    }
+}
