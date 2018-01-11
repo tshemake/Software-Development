a97cc01251881a3705e938731644c8dc287807b7
analyst <tshemake@gmail.com>
2018-01-11 22:13:23 +0500

    762/29563/step_4

---
+++ stepik/762/29563/step_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * В директории /home/box/testdir дан набор тестовых файлов.
+ *
+ * 1. Выведите в файл ~/answer1 все имена файлов, состоящие из 6
+ * символов.
+ *
+ * 2. Выведите в файл ~/answer2 все имена файлов, которые не
+ * заканчиваются на английскую букву A.
+ *
+ * Для вывода имен файлов используйте команду ls с аргументами -f1;
+ * указывайте полный абсолютный путь к директории testdir.
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LANG=C");
+            Console.WriteLine("ls -f1 /home/box/testdir/?????? > answer1");
+            Console.WriteLine("ls -f1 /home/box/testdir/*[!A] > answer2");
+        }
+    }
+}
