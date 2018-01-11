622f3808e47f2808c09e4b726d62aeaef21da1c8
analyst <tshemake@gmail.com>
2018-01-11 22:13:04 +0500

    762/29561/step_4

---
+++ stepik/762/29561/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какой символ в качестве префикса предотвращает сохранение
+ * команд в истории командной оболочки?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("пробел");
+        }
+    }
+}
