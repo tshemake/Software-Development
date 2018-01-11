c818572b77af9251d47de527373c2ca2b9881678
analyst <tshemake@gmail.com>
2018-01-11 23:27:26 +0500

    2621/46650/step_3

---
+++ stepik/2621/46650/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Для чего атрибут style?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для оформления того или иного элемента web-страницы");
+        }
+    }
+}
