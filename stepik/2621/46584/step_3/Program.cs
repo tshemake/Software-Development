a761c9eb38376ee26aec35ff622cba4ed34594a7
analyst <tshemake@gmail.com>
2018-01-11 23:25:58 +0500

    2621/46584/step_3

---
+++ stepik/2621/46584/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Навигация -
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Система ссылок для перехода с любой страницы сайта на любую другую");
+        }
+    }
+}
