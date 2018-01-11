d2416db871f2f959b092b6fa8b72d9923f486bdc
analyst <tshemake@gmail.com>
2018-01-11 23:04:33 +0500

    67/2413/step_8

---
+++ stepik/67/2413/step_8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+ * Найдите результат выражения для заданных значений a и b.
+ * Учитывайте регистр символов при ответе.
+ *
+ *  a = True
+ *  b = False
+ *  a and b or not a and not b
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(a && b || !a && !b);
+        }
+    }
+}
