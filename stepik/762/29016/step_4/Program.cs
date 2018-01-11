4dfeb448f080787cc90b0154b3b40a08aa1eb015
analyst <tshemake@gmail.com>
2018-01-11 22:12:30 +0500

    762/29016/step_4

---
+++ stepik/762/29016/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * С помощью каких символов можно осуществить многоуровневое
+ * встраивание командных оболочек?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("$(...)");
+        }
+    }
+}
