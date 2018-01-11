d3d8ae37642874445c6df9946ec1e17c757e7367
analyst <tshemake@gmail.com>
2018-01-11 23:30:06 +0500

    2621/47839/step_3

---
+++ stepik/2621/47839/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Характеристика, отвечающая за позиционирование слоя
+ * относительно остального содержимого web-страницы:
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("z-index");
+        }
+    }
+}
