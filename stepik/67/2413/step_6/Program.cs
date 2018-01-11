26e18a2b1a18712a2b8a5db2fe37d268003af3f5
analyst <tshemake@gmail.com>
2018-01-11 23:04:26 +0500

    67/2413/step_6

---
+++ stepik/67/2413/step_6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Расставьте скобки в выражении 
+ *
+ * a and b or not a and not b
+ *
+ * в соответствии с порядком вычисления выражения(приоритетом
+ * операций). Всего потребуется 5 пар скобок (внешние скобки входят
+ * в их число).
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("((a and b) or ((not a) and (not b)))");
+        }
+    }
+}
