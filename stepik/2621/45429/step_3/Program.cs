8dfb2e845685bbf5d4f45afcb013c3ee3edbb422
analyst <tshemake@gmail.com>
2018-01-11 23:18:45 +0500

    2621/45429/step_3

---
+++ stepik/2621/45429/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * За строки отвечает тег
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<tr>");
+        }
+    }
+}
