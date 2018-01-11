bf3482250c34a10e57a28e41fac3aec8fec84f15
analyst <tshemake@gmail.com>
2018-01-11 21:12:35 +0500

    762/28981/step_7

---
+++ stepik/762/28981/step_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какой знак используется для экранирования специальных
+ * символов, чтобы они не интерпретировались командной оболочкой?
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\");
+        }
+    }
+}
