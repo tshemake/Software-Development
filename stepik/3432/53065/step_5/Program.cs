b8aa31f9b771ed50804aae54ca36c32d118ab957
analyst <tshemake@gmail.com>
2018-01-12 21:36:29 +0500

    3432/53065/step_5

---
+++ stepik/3432/53065/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Mark all options for the correct function definition
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("function Function(a) { document.write(a); };");
+            Console.WriteLine("function myFunction() { };");
+            Console.WriteLine("var myFunction = function(a) {document.write(a); } ;");
+        }
+    }
+}
