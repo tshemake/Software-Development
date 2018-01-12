df4784be8ba1fe9f590924c8f619601b53aa0c8f
analyst <tshemake@gmail.com>
2018-01-12 20:17:40 +0500

    3432/53060/step_2

---
+++ stepik/3432/53061/step_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+ * Now let's check how closely you read the previous step.
+ * Mark all valid namings  for the variable.
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ExAmple");
+            Console.WriteLine("EXAMPLE");
+            Console.WriteLine("_example");
+            Console.WriteLine("Example");
+            Console.WriteLine("example");
+            Console.WriteLine("$example");
+            Console.WriteLine("ex_ample");
+        }
+    }
+}
