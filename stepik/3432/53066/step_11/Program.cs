9f4b56a158fda0ae8d0641119859d848c60118b5
analyst <tshemake@gmail.com>
2018-01-12 21:41:58 +0500

    3432/53066/step_11

---
+++ stepik/3432/53066/step_11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Now let's see how clear the previous material was!
+ */
+
+namespace step_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Function function Person (name, age, year) {this.name = name;} will create a constructor for the person");
+            Console.WriteLine("The this keyword is used to access the object from which it is called");
+            Console.WriteLine("If you assign any code to a property, then we get a method");
+        }
+    }
+}
