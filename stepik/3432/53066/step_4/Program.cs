22e33ecb5386ad5292d8d3686b2203ee17cd165d
analyst <tshemake@gmail.com>
2018-01-12 21:41:58 +0500

    3432/53066/step_4

---
+++ stepik/3432/53066/step_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Let's quickly go over the basic concepts described in the
+ * previous steps.
+ * Match the parts of statements in the most correct way.
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Every object can contain propertyes and methods");
+            Console.WriteLine("A certain value for an object is called property");
+            Console.WriteLine("Part of the code assigned to the object property is method");
+            Console.WriteLine("You can pass to the method an argument");
+            Console.WriteLine("The method must necessarily enter an object");
+        }
+    }
+}
