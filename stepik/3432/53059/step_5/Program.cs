68f1c85d7d5b63b1d827ff83212535e7487fa151
analyst <tshemake@gmail.com>
2018-01-12 19:50:13 +0500

    3432/53059/step_5

---
+++ stepik/3432/53059/step_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * 
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DOM - This model is a structuring HTML tag and allows you to access them at a unique address");
+            Console.WriteLine("BOM - This model is a structuring element of the browser and allows you to access them at a unique address");
+            Console.WriteLine("JQuery - JavaScript library for simplifying JavaScript and HTML interaction");
+            Console.WriteLine("JavaScript - Has an automatic garbage collection system");
+            Console.WriteLine("C language - Does not have an automatic garbage collection system");
+        }
+    }
+}
