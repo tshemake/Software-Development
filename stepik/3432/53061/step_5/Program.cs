f7e28593e02f6b94dfedc70e10f7a45f6a36a1ef
analyst <tshemake@gmail.com>
2018-01-12 20:17:40 +0500

    3432/53060/step_5

---
+++ stepik/3432/53061/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * In this test, you need to select all the correct options for
+ * declaring and initializing variables.
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("var x=11;");
+            Console.WriteLine("x = 22;");
+        }
+    }
+}
