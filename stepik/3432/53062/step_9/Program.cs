670a84a88e262c9cc26f832a7fb39b57b51ce331
analyst <tshemake@gmail.com>
2018-01-12 20:56:09 +0500

    3432/53062/step_9

---
+++ stepik/3432/53062/step_9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Mark all the answers that are correct, from your point of view,
+ * at a value of x equal to 7.
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Result of x == 8 is false");
+            Console.WriteLine("Result of x > 8 is false");
+            Console.WriteLine("Result of x <= 7 is true");
+            Console.WriteLine("Result of x != 8 is true");
+            Console.WriteLine("Result of x >= 7 is true");
+        }
+    }
+}
