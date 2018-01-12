82c0efb583c77aa17b786d7399554ef7599c7a73
analyst <tshemake@gmail.com>
2018-01-12 21:14:05 +0500

    3432/53064/step_8

---
+++ stepik/3432/53064/step_8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/*
+ * In this task, you need to calculate the sum of all the odd
+ * numbers occurring in a row from 1 to the number (inclusive) that
+ * is passed to our function (variable "a").
+ *
+ * Sample Input:
+ *
+ *  7
+ * Sample Output:
+ *
+ *  16
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testWhile(7));
+        }
+
+        private static double testWhile(double a)
+        {
+            double x = 0;
+            while (a > 0)
+            {
+                if (a % 2 != 0)
+                {
+                    x += a;
+                }
+                a--;
+            }
+            return x;
+        }
+    }
+}
---
+++ stepik/3432/53064/step_8/script.js
@@ -0,0 +1,10 @@
+function testWhile(a) {
+    var x = 0;
+    while (a > 0) {
+        if (a % 2 != 0) {
+            x += a;
+        }
+        a--;
+    }
+    return x;
+}
