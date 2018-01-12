3a50b7cc98ef0a56d2f69b02225db2b7d7dfc788
analyst <tshemake@gmail.com>
2018-01-12 19:49:55 +0500

    3432/53059/step_2

---
+++ stepik/3432/53059/step_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * In which year did JavaScript get its name?
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1995");
+        }
+    }
+}
