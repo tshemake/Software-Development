29375a9666586fc2e6c5ace2e50253af8e8be3b8
analyst <tshemake@gmail.com>
2018-01-12 19:50:00 +0500

    3432/53059/step_3

---
+++ stepik/3432/53059/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Who is owner of JavaScript trademark?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oracle Corporation");
+        }
+    }
+}
