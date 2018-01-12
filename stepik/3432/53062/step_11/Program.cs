71f222fde66dfec47a74424462f57064ceda3f2c
analyst <tshemake@gmail.com>
2018-01-12 20:56:10 +0500

    3432/53062/step_11

---
+++ stepik/3432/53062/step_11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Let's check how clearly the material of the previous step was
+ * described!
+ */
+
+namespace step_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("With x = 3 and y = 6 the result of x == 3 || y > 6 operation will be true");
+            Console.WriteLine("With x = 3 and y = 6 the result of !(x > y) operation will be true");
+            Console.WriteLine("With x = 3 and y = 6 the result of x < 3 && y == 6 operation will be false");
+        }
+    }
+}
