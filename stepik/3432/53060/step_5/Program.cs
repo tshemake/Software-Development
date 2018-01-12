bebe7706c5dd6cf54b851d44913ec8693a1c8838
analyst <tshemake@gmail.com>
2018-01-12 20:03:04 +0500

    3432/53060/step_5

---
+++ stepik/3432/53060/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Let's see how you learned this lesson! Check the boxes opposite
+ * to the correct statements!
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You can conclude a sequence of statements that are executed together in curly brackets.");
+            Console.WriteLine("The JavaScript code in the HTML file should be framed by the <script> </script> tag");
+        }
+    }
+}
