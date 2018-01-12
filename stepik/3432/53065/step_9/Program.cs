70cf49a6d1d48831f0fa43d6542bd2232edc3d5f
analyst <tshemake@gmail.com>
2018-01-12 21:36:29 +0500

    3432/53065/step_9

---
+++ stepik/3432/53065/step_9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * In this test, it is necessary to note all the correct definitions.
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A function that has a name can call itself");
+            Console.WriteLine("The function can be defined and immediately called within a single command");
+            Console.WriteLine("The \"return\" command does not need to be used in a function");
+            Console.WriteLine("The return command that returns a result can be used in a function more than once");
+        }
+    }
+}
