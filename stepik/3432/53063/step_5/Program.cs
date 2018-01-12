fd8b29c71ac20688f288218dc64a0da29f10b2a9
analyst <tshemake@gmail.com>
2018-01-12 21:06:35 +0500

    3432/53063/step_5

---
+++ stepik/3432/53063/step_5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*
+ * In this task, you need to write in the specified place the code
+ * that will assign the value of the sum of the variables "a" and "b"
+ * to the variable "x" in case a > b, the difference "a" and "b" if
+ * a < b and their doubled product in other cases.
+ * Sample Input:
+ *
+ *  0 5
+ * Sample Output:
+ *
+ *  -5
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testIf(0, 5));
+        }
+
+        private static double testIf(double a, double b)
+        {
+            double x;
+            if (a > b)
+            {
+                x = a + b;
+            }
+            else if (a < b)
+            {
+                x = a - b;
+            }
+            else
+            {
+                x = a * b;
+            }
+            return x;
+        }
+    }
+}
