18fecfb7d440e480872768c018f42e90ebef7415
analyst <tshemake@gmail.com>
2018-01-12 20:56:09 +0500

    3432/53062/step_6

---
+++ stepik/3432/53062/step_6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Compare the results of calculating the assignment operations for
+ * the initial values x = 10 and y = 5;
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x += y; x = 15");
+            Console.WriteLine("x -= y; x = 5");
+            Console.WriteLine("x *= y; x = 50");
+            Console.WriteLine("x /= y; x = 2");
+            Console.WriteLine("x %= y; x = 0");
+
+        }
+    }
+}
