e48b35ee613f0fb9325bf8af909e33ea70baeb05
analyst <tshemake@gmail.com>
2018-01-13 14:55:24 +0500

    3432/53071/step_2

---
+++ stepik/3432/53071/step_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * How do you think, what can we do with exception?
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("All listed options");
+        }
+    }
+}
