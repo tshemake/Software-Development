b5bf9e6680a7b13faff612447cd45ce14ef55dbb
analyst <tshemake@gmail.com>
2018-01-13 14:17:27 +0500

    3432/53067/step_8

---
+++ stepik/3432/53067/step_8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ * In this task, two lines are passed to our testStr function.
+ * You need to return a string from the function that will include
+ * both of these lines, converting all the letters to lowercase in
+ * the first line, and to capital letters in the second.
+ * Sample Input:
+ *
+ *  Hello World!
+ * Sample Output:
+ *
+ *  helloWORLD!
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testStr("Hello", "World!"));
+        }
+
+        private static string testStr(string a, string b)
+        {
+            return a.ToLower() + b.ToUpper();
+        }
+    }
+}
---
+++ stepik/3432/53067/step_8/script.js
@@ -0,0 +1,3 @@
+function testStr(a, b) {
+    return a.toLowerCase() + b.toUpperCase();
+}
