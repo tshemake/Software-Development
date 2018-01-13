f0278d095458ae3b7358a6ea2b4b84926193735b
analyst <tshemake@gmail.com>
2018-01-13 15:21:59 +0500

    3432/53074/step_6

---
+++ stepik/3432/53074/step_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+ * And now - the programming task!)
+ * The text below describes an anonymous function inside which is a
+ * closure that changes the value of a variable in an external
+ * function and returns it. But here's the problem - the lines are
+ * messed up in places! Restore the necessary sequence of lines.
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("var enumerator = (function () {");
+            Console.WriteLine("var actual = 0;");
+            Console.WriteLine("return function () {");
+            Console.WriteLine("actual++;");
+            Console.WriteLine("return actual;");
+            Console.WriteLine("}");
+            Console.WriteLine("})();");
+        }
+    }
+}
