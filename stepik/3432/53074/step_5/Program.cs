c194738c1efb5a7c59c569cbd7bea1e6eceaea5c
analyst <tshemake@gmail.com>
2018-01-13 15:21:59 +0500

    3432/53074/step_5

---
+++ stepik/3432/53074/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * For starters - a small test!
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A closure stores a reference to variables that are in a function external to it");
+            Console.WriteLine("If inside the normal function, nested in another function, make a call to the input parameter of the external function, then it will be called the closure");
+            Console.WriteLine("If inside the normal function, nested in another function, make a call to a variable in the external function, then it will be called the closure");
+        }
+    }
+}
