b6b555a2997d4cc474127088e3b9a7b25ddccc87
analyst <tshemake@gmail.com>
2018-01-13 15:18:02 +0500

    3432/53073/step_10

---
+++ stepik/3432/53073/step_10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Let's check that we have learned about the RegExp object.
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The RegExp object constructor can accept either one or two arguments");
+            Console.WriteLine("The RegExp object has five properties");
+        }
+    }
+}
