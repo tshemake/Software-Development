ca66d29648b5a55997d3328df1b05fc3a3f517d3
analyst <tshemake@gmail.com>
2018-01-13 14:49:46 +0500

    3432/53070/step_5

---
+++ stepik/3432/53070/step_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ * And then you need to calculate and return the area of the
+ * triangle from the function. The arguments "a" and "b" passed
+ * to the function are the lengths of the sides, and "c" is the
+ * angle between them in degrees.
+ * Sample Input:
+ *
+ *  10 8 29
+ * Sample Output:
+ *
+ *  19.392384809853482
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testMath(10, 8, 29));
+        }
+
+        private static double testMath(int a, int b, int c)
+        {
+            return 1 * a * b * Math.Sin((Math.PI / 180) * c) / 2;
+        }
+    }
+}
---
+++ stepik/3432/53070/step_5/script.js
@@ -0,0 +1,3 @@
+function testMath(a, b, c) {
+    return 1 * a * b * Math.sin((Math.PI / 180) * c) / 2;
+}
