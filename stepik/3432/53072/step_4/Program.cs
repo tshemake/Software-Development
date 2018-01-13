f413bcd4a64543c0ce4b7e4e3658f6f57b4f2c1e
analyst <tshemake@gmail.com>
2018-01-13 15:04:32 +0500

    3432/53072/step_4

---
+++ stepik/3432/53072/step_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Now warming up task). 
+ * Specify regular expressions that match some substring in the
+ * string "http://www.stepik.org";
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/\\/\\//");
+            Console.WriteLine("/www/");
+            Console.WriteLine("/\\//");
+        }
+    }
+}
