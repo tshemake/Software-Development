edfa8abbdbbdccf3247c15d33408c7bd1db9b8d7
analyst <tshemake@gmail.com>
2018-01-13 15:04:33 +0500

    3432/53072/step_6

---
+++ stepik/3432/53072/step_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Mark the expressions that match the lines or substrings.
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/\\d/ for string \"123qwerty456\"");
+            Console.WriteLine("/[^qwerty]/ for string \"123qwerty456\"");
+            Console.WriteLine("/[4-6]/ for string \"123qwerty456\"");
+            Console.WriteLine("/q/ for string \"qwerty\"");
+            Console.WriteLine("/qwerty/ for string \"123qwerty456\"");
+            Console.WriteLine("/qwerty/ for string \"qwerty\"");
+        }
+    }
+}
