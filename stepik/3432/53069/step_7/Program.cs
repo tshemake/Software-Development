b997b699e291a402010bd8c229f1880ffa007244
analyst <tshemake@gmail.com>
2018-01-13 14:36:38 +0500

    3432/53069/step_7

---
+++ stepik/3432/53069/step_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Before the final task of this lesson, we answer some simple
+ * questions to verify the comprehensibility of the material.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Objects of type Date can be compared among themselves");
+            Console.WriteLine("The getDay () method returns the day of the week of the date");
+            Console.WriteLine("After declaring the variable var myDate = new Date (); the variable will be the current date and time");
+        }
+    }
+}
