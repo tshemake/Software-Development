c46d806b509253acc17e523d670b030aa4a9eade
analyst <tshemake@gmail.com>
2018-01-13 14:55:24 +0500

    3432/53071/step_5

---
+++ stepik/3432/53071/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Choose the right options
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You can throw almost any type of data as an exception");
+            Console.WriteLine("The exception is caught by the catch command");
+            Console.WriteLine("When handling an exception, there must be at least one catch or finally block");
+        }
+    }
+}
