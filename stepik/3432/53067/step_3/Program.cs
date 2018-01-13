c844d541ec721d73a6e72b0746bb88d8d98cf1e8
analyst <tshemake@gmail.com>
2018-01-13 14:17:27 +0500

    3432/53067/step_3

---
+++ stepik/3432/53067/step_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+ * In this task, two lines are passed to our testStr function.
+ * You need to return from the function their total length.
+ * Sample Input:
+ *
+ *  498859 649838
+ * Sample Output:
+ *
+ *  12
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testStr("498859", "649838"));
+        }
+
+        private static double testStr(string a, string b)
+        {
+            return a.Length + b.Length;
+        }
+    }
+}
---
+++ stepik/3432/53067/step_3/script.js
@@ -0,0 +1,3 @@
+function testStr(a, b) {
+    return a.length + b.length;
+}
