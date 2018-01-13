2bda3eb6c8c4094e24cc3b40a7b5e20b9aeab3f5
analyst <tshemake@gmail.com>
2018-01-13 14:49:46 +0500

    3432/53070/step_9

---
+++ stepik/3432/53070/step_9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*
+ * In this task, you need to build the variable a to the power of
+ * b and return from the function the remainder of the division
+ * resulted value by 3.
+ * Sample Input:
+ *
+ *  2 6
+ * Sample Output:
+ *
+ *  1
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testMath(2, 6));
+        }
+
+        private static double testMath(double a, double b)
+        {
+            return Math.Pow(a, b) % 3;
+        }
+    }
+}
