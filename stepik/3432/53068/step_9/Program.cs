e4fdb51a05750e7e2107c5e461e3ad8cd2efb3dc
analyst <tshemake@gmail.com>
2018-01-13 14:27:45 +0500

    3432/53068/step_9

---
+++ stepik/3432/53068/step_9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * As always at the end of the lesson, first a small test!
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The only method that allows you to add elements to the middle of an array is the splice()");
+            Console.WriteLine("The numbering of elements in the array begins with 0");
+            Console.WriteLine("The pop() method removes one element from the array - the last one");
+        }
+    }
+}
