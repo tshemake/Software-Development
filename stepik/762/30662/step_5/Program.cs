616fbb394f0381182272f9cc085c20cd3c009675
analyst <tshemake@gmail.com>
2018-01-14 18:10:31 +0500

    762/30662/step_5

---
+++ stepik/762/30662/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Сделайте так, чтобы сценарий исполнялся в командной оболочке
+ * Korn shell
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#!/bin/ksh");
+            Console.WriteLine("echo Antwerp");
+        }
+    }
+}
