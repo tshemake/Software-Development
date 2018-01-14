bf0c3b7261c8565c07cea90393b96a97ac433d6b
analyst <tshemake@gmail.com>
2018-01-14 18:42:14 +0500

    762/31368/step_5

---
+++ stepik/762/31368/step_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какая встроенная команда командной оболочки используется для
+ * вычисления значений арифметических выражений?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("let");
+        }
+    }
+}
