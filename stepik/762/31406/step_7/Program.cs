dc1693fe7ff0a3b1d9fb12052e83ea8f8f68868c
analyst <tshemake@gmail.com>
2018-01-14 23:49:22 +0500

    762/31406/step_7

---
+++ stepik/762/31406/step_7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+ * 1. Разблокируйте учетную запись пользователя foo.
+ *
+ * 2. Заблокируйте учетную запись пользователя bar.
+ *
+ * 3. Задайте срок действия учетной записи пользователя foo -
+ * 1 апреля 2019 года.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sudo usermod -U foo");
+            Console.WriteLine("sudo usermod -L bar");
+            Console.WriteLine("sudo chage -E 2019-04-01 foo");
+        }
+    }
+}
