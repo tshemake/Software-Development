cd559217c54bf2367405c33b89c57e36cdb95935
analyst <tshemake@gmail.com>
2018-01-14 23:17:44 +0500

    762/31390/step_4

---
+++ stepik/762/31390/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какой пользователь является наиболее привилегированным в ОС Linux?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("root");
+        }
+    }
+}
