78c1d0afeab4df6b1c6f6ed40c86a07d9976b16e
analyst <tshemake@gmail.com>
2018-01-14 23:49:22 +0500

    762/31406/step_6

---
+++ stepik/762/31406/step_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Для блокировки учетной записи пользователя необходимо перед
+ * хэшем пароля в /etc/shadow добавить
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!");
+        }
+    }
+}
