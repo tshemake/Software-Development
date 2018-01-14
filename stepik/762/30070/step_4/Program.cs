0f0b9440f85add46fa36a211e562e2737cffba6d
analyst <tshemake@gmail.com>
2018-01-14 18:01:07 +0500

    762/30070/step_4

---
+++ stepik/762/30070/step_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Какие команды позволяют начать вводить текст (перейти в режим
+ * ввода текста)?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("o");
+            Console.WriteLine("i");
+            Console.WriteLine("a");
+        }
+    }
+}
