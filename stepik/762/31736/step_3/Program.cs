6fde56ac90acf28b8dfdd6ce9c36ebf2396a0f9f
analyst <tshemake@gmail.com>
2018-01-15 00:31:20 +0500

    762/31736/step_3

---
+++ stepik/762/31736/step_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Какой файл сценария командной оболочки bash можно использовать
+ * для объявления псевдонимов команд, функций и переменных для всех
+ * пользователей системы?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/etc/profile");
+        }
+    }
+}
