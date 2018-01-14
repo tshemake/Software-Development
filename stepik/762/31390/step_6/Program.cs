fc6476df01c75684c264358212ef843f2fdb69f3
analyst <tshemake@gmail.com>
2018-01-14 23:17:44 +0500

    762/31390/step_6

---
+++ stepik/762/31390/step_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какая директория выступает в роли шаблона домашней директории
+ * и стандартного профиля пользователя?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/etc/skel");
+        }
+    }
+}
