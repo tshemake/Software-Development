58c9ad636a4afca7c24cb93edc13b1b1114e2295
analyst <tshemake@gmail.com>
2018-01-15 01:05:55 +0500

    762/31781/step_6

---
+++ stepik/762/31781/step_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Членство пользователя в группах может быть изменено с помощью
+ * утилиты
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("usermod");
+        }
+    }
+}
