dd2e2cc4bea19062e97dba9124fe29c2a8117b2e
analyst <tshemake@gmail.com>
2018-01-14 23:17:44 +0500

    762/31390/step_7

---
+++ stepik/762/31390/step_7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+ * 1. Создайте пользователя user01, с домашней директорией
+ * /home/u01d и командной оболочкой по умолчанию Korn shell.
+ *
+ * 2. Удалите учетные записи пользователей, которые используют
+ * командную оболочку C shell.
+ *
+ * 3. Удалите учетную запись и домашнюю директорию пользователя
+ * test02.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sudo useradd -m -d /home/u01d -s /bin/ksh user01");
+            Console.WriteLine("sudo userdel `grep /bin/csh$ /etc/passwd | cut -d: -f1`");
+            Console.WriteLine("sudo userdel -r test02");
+        }
+    }
+}
