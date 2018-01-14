5a13fcb025035d9e98b09f2c7cfe3ad0106d2cfc
analyst <tshemake@gmail.com>
2018-01-14 18:39:16 +0500

    762/30719/step_5

---
+++ stepik/762/30719/step_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * С помощью какой команды можно получить параметры функционирования
+ * командной оболочки?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("shopt");
+        }
+    }
+}
