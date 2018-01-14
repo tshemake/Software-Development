83b99cd8d3edb61615c025a677f42f5e38c455bf
analyst <tshemake@gmail.com>
2018-01-15 00:31:20 +0500

    762/31736/step_4

---
+++ stepik/762/31736/step_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Какие файлы сценариев командной оболочки bash можно использовать
+ * для объявления псевдонимов команд, функций и переменных для
+ * конкретного пользователя системы?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("~/.profile");
+            Console.WriteLine("~/.bash_profile");
+            Console.WriteLine("~/.bashrc");
+        }
+    }
+}
