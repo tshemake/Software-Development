0590816a19e97bdd29216f0784c2fac02aabbcf6
analyst <tshemake@gmail.com>
2018-01-14 18:00:44 +0500

    762/30012/step_5

---
+++ stepik/762/30012/step_5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+ * Содержимое файла tennis.txt:
+ *  box@de98796:~$ cat tennis.txt
+ *  Amelie Mauresmo, Fra
+ *  Kim Clijsters, BEL
+ *  Justine Henin, Bel
+ *  Serena Williams, usa
+ *  Venus Williams, USA
+ * Нужно найти всех спортсменок из Бельгии.
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("grep -i Bel tennis.txt");
+        }
+    }
+}
