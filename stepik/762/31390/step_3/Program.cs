ac96569fc9ab4aa287e3f57e9bd950f9f3765d7f
analyst <tshemake@gmail.com>
2018-01-14 23:17:43 +0500

    762/31390/step_3

---
+++ stepik/762/31390/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Где  расположена локальная база данных учетных записей
+ * пользователей в Linux?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/etc/passwd");
+        }
+    }
+}
