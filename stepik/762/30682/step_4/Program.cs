af811baf1e1153ed72dd8a0ce4991ac6b6a3dee0
analyst <tshemake@gmail.com>
2018-01-14 18:32:28 +0500

    762/30682/step_4

---
+++ stepik/762/30682/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С помощью каких символов можно заменить команду test?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[ ]");
+        }
+    }
+}
