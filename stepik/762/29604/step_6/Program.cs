1b175e5d2de95f790850eaa5452f0716edfb7e6a
analyst <tshemake@gmail.com>
2018-01-14 17:59:07 +0500

    762/29604/step_6

---
+++ stepik/762/29604/step_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * В каких случаях сообщения об ошибках не будут выводиться на экран?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("find / > allfiles.txt 2> /dev/null");
+            Console.WriteLine("find / > allfiles.txt 2>&1");
+        }
+    }
+}
