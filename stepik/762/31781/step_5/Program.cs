9c727960230e2097a4ab63bc63fb58d24fabac3f
analyst <tshemake@gmail.com>
2018-01-15 01:05:54 +0500

    762/31781/step_5

---
+++ stepik/762/31781/step_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С помощью команды groups можно
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("посмотреть в каких группах состоит пользователь");
+        }
+    }
+}
