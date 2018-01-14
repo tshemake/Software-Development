e8c73b0aecbaff53b7f3280c5904cdcce0c012e8
analyst <tshemake@gmail.com>
2018-01-14 18:00:45 +0500

    762/30012/step_9

---
+++ stepik/762/30012/step_9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Вставьте пропущенные строки:
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("box@de98796:~$ echo Заморский | sed 's/ский/ить/'");
+            Console.WriteLine("Заморить");
+        }
+    }
+}
