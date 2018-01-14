dd8ac009eb2c1860c1c7c25628d7384af6c52e29
analyst <tshemake@gmail.com>
2018-01-14 23:49:21 +0500

    762/31406/step_3

---
+++ stepik/762/31406/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * В каких файлах хранятся пароли в зашифрованном виде?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/etc/shadow");
+        }
+    }
+}
