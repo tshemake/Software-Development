e7d82b43163ecfe418c574e3d99d8ec6f794ff67
analyst <tshemake@gmail.com>
2018-01-14 18:01:07 +0500

    762/30070/step_9

---
+++ stepik/762/30070/step_9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Редактор vi находится в режиме ввода команд. Заполните пропуски,
+ * чтобы заменить строки baz на строки bar начиная с 2 и заканчивая 5
+ * строкой файла.
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(":2,5 s/baz/bar/g");
+        }
+    }
+}
