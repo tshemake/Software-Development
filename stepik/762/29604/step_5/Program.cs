424254e5f2ceb5b17d2efc9d15b2cba9b7b85ca5
analyst <tshemake@gmail.com>
2018-01-14 17:59:07 +0500

    762/29604/step_5

---
+++ stepik/762/29604/step_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * В каких случаях содержимое файла будет удалено перед записью новой информации?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("echo Hello world! >|hello");
+            Console.WriteLine("echo Hello world! >hello");
+        }
+    }
+}
