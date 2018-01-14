4b4d2fdaacb023c199ea5486da24c1fb67507c02
analyst <tshemake@gmail.com>
2018-01-14 17:59:51 +0500

    762/29652/step_6

---
+++ stepik/762/29652/step_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+ * Необходимо отсортировать содержимое tennis.txt по второму
+ * столбцу.
+ *
+ *  box@de98796:~$ cat tennis.txt
+ *  Amelie Mauresmo, Fra
+ *  Kim Clijsters, BEL
+ *  Justine Henin, Bel
+ *  Serena Williams, usa
+ *  Venus Williams, USA
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sort -k2 tennis.txt");
+        }
+    }
+}
