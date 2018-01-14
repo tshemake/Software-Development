bfc1b4e862a43a82eecc65bfcd735476751c5bf0
analyst <tshemake@gmail.com>
2018-01-14 18:48:20 +0500

    762/31369/step_7

---
+++ stepik/762/31369/step_7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * По умолчанию при смене пользователя утилита su
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("сохраняет переменные окружения командной оболочки исходного пользователя");
+        }
+    }
+}
