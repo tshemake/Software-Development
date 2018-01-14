eb9ad208acd254459c28fcb53a2f3ad868d12434
analyst <tshemake@gmail.com>
2018-01-14 17:59:51 +0500

    762/29652/step_7

---
+++ stepik/762/29652/step_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * 1. Сохраните в файл ~/bashusers отсортированный в алфавитном
+ * порядке список имен пользователей из /etc/passwd, которые
+ * используют командную оболочку bash.
+ * 2. Рассмотрите вывод утилиты /sbin/ifconfig. Создайте команду,
+ * с помощью которой будут выводиться исключительно IPv4-адреса.
+ * Сохраните результат в файл ~/ipaddr.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("grep bash /etc/passwd | cut -d: -f1 | sort > ~/bashusers");
+            Console.WriteLine("sbin/ifconfig | grep -o 'addr:[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}' | sed 's/addr://' > ipaddr");
+        }
+    }
+}
