5368e1781c1e665b641a1af19a80868c3ddb39c3
analyst <tshemake@gmail.com>
2018-01-14 18:00:44 +0500

    762/30012/step_6

---
+++ stepik/762/30012/step_6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/*
+ * Содержимое bar.txt:
+ *  box@de98796:~$ cat bar.txt
+ *  br
+ *  bar
+ *  baar
+ *  baaar
+ * Подставьте регулярные выражения, соответствующие результатам
+ * выполнения команд:
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("box@de98796:~$ grep -E \"a*\" bar.txt");
+            Console.WriteLine("br");
+            Console.WriteLine("bar");
+            Console.WriteLine("baar");
+            Console.WriteLine("baaar");
+            Console.WriteLine();
+            Console.WriteLine("box@de98796:~$ grep -E \"a+\" bar.txt");
+            Console.WriteLine("bar");
+            Console.WriteLine("baar");
+            Console.WriteLine("baaar");
+            Console.WriteLine();
+            Console.WriteLine("box@de98796:~$ grep -E 'aaa?' bar.txt");
+            Console.WriteLine("baar");
+            Console.WriteLine("baaar");
+        }
+    }
+}
