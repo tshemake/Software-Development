af1584087ab4baebfd0468cbaa17365cc5228389
analyst <tshemake@gmail.com>
2018-01-14 18:00:21 +0500

    762/29785/step_7

---
+++ stepik/762/29785/step_7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Найти все *.conf файлы, расположенные в директории /etc и
+ * скопировать их в /backup (тип файлов -type в команде  указывать
+ * не нужно):
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("find /etc -name '*.conf' -exec cp {} /backup \\;");
+        }
+    }
+}
