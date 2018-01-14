2e74b8243a43fce07fba3fe8af5665b357d8e562
analyst <tshemake@gmail.com>
2018-01-14 18:32:29 +0500

    762/30682/step_10

---
+++ stepik/762/30682/step_10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Нужно завершить цикл until, как только j станет меньше 3.
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"#!/bin/bash
+j=9
+until [ $j -lt 3 ]
+do
+    let j=j-1
+done");
+        }
+    }
+}
---
+++ stepik/762/30682/step_10/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+j=9
+until [ $j -lt 3 ]
+do
+    let j=j-1
+done
