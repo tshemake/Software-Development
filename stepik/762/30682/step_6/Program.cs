8d70a5724e5e2becff7fd7a866ce4141e316a75a
analyst <tshemake@gmail.com>
2018-01-14 18:32:29 +0500

    762/30682/step_6

---
+++ stepik/762/30682/step_6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/*
+ * В результате выполнения скрипта
+ *
+ *  #!/bin/bash
+ *  count=11
+ *  if [ $count -ge 42 ]
+ *  then
+ *    echo "Достаточно."
+ *  else
+ *    echo "Мало."
+ *  fi
+ *
+ * на экран будет выведено
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 11;
+            if (count >= 42)
+            {
+                Console.WriteLine("Достаточно.");
+            }
+            else
+            {
+                Console.WriteLine("Мало.");
+            }
+        }
+    }
+}
