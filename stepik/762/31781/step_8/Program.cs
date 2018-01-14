6e277a1cbb99a4917a291bfdae90cabbe26c6b17
analyst <tshemake@gmail.com>
2018-01-15 01:05:55 +0500

    762/31781/step_8

---
+++ stepik/762/31781/step_8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С помощью какой утилиты вы можете изменить имя группы?
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("groupmod");
+        }
+    }
+}
