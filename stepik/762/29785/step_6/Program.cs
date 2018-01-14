8a7dc010b42e70623e97d86e6b64febedacb54d8
analyst <tshemake@gmail.com>
2018-01-14 18:00:21 +0500

    762/29785/step_6

---
+++ stepik/762/29785/step_6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Найти все файлы, соответствующие шаблону '*.bar':
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("find / -type f -name '*.bar'");
+        }
+    }
+}
