28a9179dbb5e4c16a7d2d4fd36b278ad3c8e9c11
analyst <tshemake@gmail.com>
2018-01-14 17:59:51 +0500

    762/29652/step_5

---
+++ stepik/762/29652/step_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Необходимо подсчитать строки в выводе утилиты env:
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("env | wc -l");
+        }
+    }
+}
