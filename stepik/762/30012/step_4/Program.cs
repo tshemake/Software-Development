9b380e139355a9793681b0d637e0cfdd803bbd50
analyst <tshemake@gmail.com>
2018-01-14 18:00:44 +0500

    762/30012/step_4

---
+++ stepik/762/30012/step_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * Содержимое файла tennis.txt:
+ *  box@de98796:~$ cat tennis.txt
+ *  Amelie Mauresmo, Fra
+ *  Kim Clijsters, BEL
+ *  Justine Henin, Bel
+ *  Serena Williams, usa
+ *  Venus Williams, USA
+ * В результате выполнения каких команд будут выведены строки:
+ *  Amelie Mauresmo, Fra
+ *  Serena Williams, usa
+ *  Venus Williams, USA
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("grep -G \"M\\|W\" tennis.txt");
+            Console.WriteLine("grep -E \"W|M\" tennis.txt");
+        }
+    }
+}
