729e42428a12f7fd609998a52ce411d72b374403
analyst <tshemake@gmail.com>
2018-01-14 18:39:16 +0500

    762/30719/step_4

---
+++ stepik/762/30719/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С помощью какой функции можно получить параметры сценария?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("getopts");
+        }
+    }
+}
