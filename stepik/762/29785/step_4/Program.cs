e209d9233c7d9e7030271f970ee4758a6562df47
analyst <tshemake@gmail.com>
2018-01-14 18:00:21 +0500

    762/29785/step_4

---
+++ stepik/762/29785/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Вы только что создали файл  loctest. Какую команду нужно ввести
+ * перед поиском, чтобы файл можно было найти с помощью locate?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("updatedb");
+        }
+    }
+}
