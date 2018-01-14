b2cf9ca5a2e7929352b0e3285754dd5f0ae15fff
analyst <tshemake@gmail.com>
2018-01-14 18:42:14 +0500

    762/31368/step_3

---
+++ stepik/762/31368/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какая команда позволяет интерпретировать переданные аргументы
+ * как директивы сценария командной оболочки?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("eval");
+        }
+    }
+}
