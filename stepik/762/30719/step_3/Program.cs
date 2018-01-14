750823223f2a5a7922cf064fa3927bcbc9d49f5c
analyst <tshemake@gmail.com>
2018-01-14 18:39:16 +0500

    762/30719/step_3

---
+++ stepik/762/30719/step_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Сопоставьте параметры с их описанием
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("$$ - идентификатор процесса (PID)");
+            Console.WriteLine("$? - последний код завершения процесса");
+            Console.WriteLine("$* - строковое представление всех аргументов");
+            Console.WriteLine("$1 - первый аргумент");
+            Console.WriteLine("$2 - второй аргумент");
+            Console.WriteLine("$# - количество аргументов");
+        }
+    }
+}
