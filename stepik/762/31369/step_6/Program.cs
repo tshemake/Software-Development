facf3d45f3ac46a3844717313bba245715cc10e4
analyst <tshemake@gmail.com>
2018-01-14 18:48:20 +0500

    762/31369/step_6

---
+++ stepik/762/31369/step_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какая утилита позволяет запустить командную оболочку от лица
+ * другого пользователя?
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("su");
+        }
+    }
+}
