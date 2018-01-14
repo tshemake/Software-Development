2ee4883d30aaf30649ccf71a683458fe30c49b94
analyst <tshemake@gmail.com>
2018-01-14 18:10:31 +0500

    762/30662/step_6

---
+++ stepik/762/30662/step_6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+ * На стандартный ввод (stdin) скрипта переданы 3 случайных
+ * числа a, b и c, разделенные пробелами.
+ *
+ * Необходимо передать числа на стандартный вывод (stdout) в
+ * порядке a, c и b, разделенные запятыми.
+ *
+ * Передать числа на стандартный вывод можно с помощью echo.
+ *
+ * Sample Input:
+ *
+ *  0 4 2
+ * Sample Output:
+ *
+ *  0,2,4
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] arguments = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("{0},{2},{1}", arguments);
+        }
+    }
+}
