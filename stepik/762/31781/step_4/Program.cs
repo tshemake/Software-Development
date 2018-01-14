43ffff0053b5ba934f07c8adb240777ab0cf98ae
analyst <tshemake@gmail.com>
2018-01-15 01:05:54 +0500

    762/31781/step_4

---
+++ stepik/762/31781/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Членство пользователей в группах описывается в файле
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/etc/group");
+        }
+    }
+}
