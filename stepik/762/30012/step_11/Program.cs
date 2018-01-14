48143079fcca608eea3e187a0aa6629a0eefd043
analyst <tshemake@gmail.com>
2018-01-14 18:00:45 +0500

    762/30012/step_11

---
+++ stepik/762/30012/step_11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+/*
+ * Напишите результат выполнения команды:
+ *
+ *  box@de98796:~$ echo 01.05.1889 | sed 's|\(..\).\(..\).\(....\)|\2/\1/\3|'
+ */
+
+namespace step_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "01.05.1889";
+            Console.WriteLine(Regex.Replace(str, "(..).(..).(....)", "$2/$1/$3"));
+        }
+    }
+}
