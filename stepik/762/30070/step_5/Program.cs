7581c1b1a8f54ba3912e686a67933474a9780fd4
analyst <tshemake@gmail.com>
2018-01-14 18:01:07 +0500

    762/30070/step_5

---
+++ stepik/762/30070/step_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Какие команды позволяют завершить работу с редактором vi с
+ * сохранением данных (внесенных изменений)?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("сначала :w, потом :q");
+            Console.WriteLine("ZZ");
+            Console.WriteLine(":wq");
+        }
+    }
+}
