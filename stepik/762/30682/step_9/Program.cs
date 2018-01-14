752c7343c2b48e16d1603d68d0dd35abefda3ef6
analyst <tshemake@gmail.com>
2018-01-14 18:32:29 +0500

    762/30682/step_9

---
+++ stepik/762/30682/step_9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Нужно завершить цикл while, как только $i превысит 11.
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"#!/bin/bash
+i=1
+while [ $i -le 11 ]
+do
+    let i=i+1
+done");
+        }
+    }
+}
---
+++ stepik/762/30682/step_9/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+i=1
+while [ $i -le 11 ]
+do
+    let i=i+1
+done
