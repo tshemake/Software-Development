81d02ac7757dc0dd884b412a04314605e15a2d08
analyst <tshemake@gmail.com>
2018-01-14 18:32:29 +0500

    762/30682/step_8

---
+++ stepik/762/30682/step_8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Необходимо вывести все целые числа в порядке от 10 до 1.
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("for i in {10..1}; do echo $i; done;");
+        }
+    }
+}
