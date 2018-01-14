864f115e22de8bb823d27e731b6d3e25fc44f48d
analyst <tshemake@gmail.com>
2018-01-14 17:59:07 +0500

    762/29604/step_7

---
+++ stepik/762/29604/step_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * С помощью каких команд можно активировать режим noclobber?
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("set -C");
+            Console.WriteLine("set -o noclobber");
+        }
+    }
+}
