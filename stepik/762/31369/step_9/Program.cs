886d3d3c9d15d836a39719d48045710547f9b089
analyst <tshemake@gmail.com>
2018-01-14 18:48:20 +0500

    762/31369/step_9

---
+++ stepik/762/31369/step_9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * С помощью какой утилиты можно  выполнить отдельную команду от
+ * имени другого пользователя (в т.ч. от имени пользователя root),
+ * не зная пароля целевого пользователя?
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sudo");
+        }
+    }
+}
