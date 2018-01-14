f3fbfd1ac8dccf56387a5bc5489454b1ff714e1c
analyst <tshemake@gmail.com>
2018-01-14 18:32:29 +0500

    762/30682/step_7

---
+++ stepik/762/30682/step_7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+ *  #!/bin/bash
+ *  a=3
+ *  b=4
+ *  if [ $a -lt $b ]
+ *  then
+ *    let c=b-a
+ *  else 
+ *   let c=a+b
+ *  fi
+ *  echo $c
+ * Какое число будет выведено на экран в результате выполнения скрипта?
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+            int c;
+            if (a < b)
+            {
+                c = b - a;
+            }
+            else
+            {
+                c = a + b;
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
