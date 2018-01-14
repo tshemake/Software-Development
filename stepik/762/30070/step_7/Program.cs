989dc471993646888ecabab3bc5ff3d317135a21
analyst <tshemake@gmail.com>
2018-01-14 18:01:07 +0500

    762/30070/step_7

---
+++ stepik/762/30070/step_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какая комбинация из 3 клавиш, использованная в режиме ввода
+ * команд, позволит создать дубликат текущей строки файла?
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yyp");
+        }
+    }
+}
