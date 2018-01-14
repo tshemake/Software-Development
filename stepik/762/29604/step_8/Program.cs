fabd642d0623161294af702dad4691a7b87ad606
analyst <tshemake@gmail.com>
2018-01-14 17:59:07 +0500

    762/29604/step_8

---
+++ stepik/762/29604/step_8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Параметр noclobber активирован?
+ *
+ *  box@de98796:~$ echo $-
+ *  himBCH
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Да");
+        }
+    }
+}
