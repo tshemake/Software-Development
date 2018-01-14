22471d3e209a64c3563f621f4103b0f8e9eabc3f
analyst <tshemake@gmail.com>
2018-01-14 18:48:20 +0500

    762/31369/step_4

---
+++ stepik/762/31369/step_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * С помощью каких команд можно получить информацию о пользователях,
+ * которые совершили вход в систему?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("who");
+            Console.WriteLine("w");
+        }
+    }
+}
