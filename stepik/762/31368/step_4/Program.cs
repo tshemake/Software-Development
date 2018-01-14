5e5f80d0553cbce3b53c3203ec20085510995073
analyst <tshemake@gmail.com>
2018-01-14 18:42:14 +0500

    762/31368/step_4

---
+++ stepik/762/31368/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Какой оператор позволяет сравнивать числовые значения?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(( ))");
+        }
+    }
+}
