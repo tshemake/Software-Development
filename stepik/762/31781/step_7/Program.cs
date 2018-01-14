3a89704524dfc1d4ca118cc9eba78377a6bfd801
analyst <tshemake@gmail.com>
2018-01-15 01:05:55 +0500

    762/31781/step_7

---
+++ stepik/762/31781/step_7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Какой параметр утилиты usermod позволяет сохранить список групп
+ * пользователя, в которых он состоял, если имена данных групп не
+ * были переданы в составе команды?
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-a");
+        }
+    }
+}
