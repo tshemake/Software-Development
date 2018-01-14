352f2add17d42df09304b68bb34070a0d73d25ee
analyst <tshemake@gmail.com>
2018-01-14 18:48:20 +0500

    762/31369/step_8

---
+++ stepik/762/31369/step_8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Для того, чтобы работать в окружении командной оболочки целевого
+ * пользователя необходимо выполнить команду
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("su -");
+        }
+    }
+}
