87c623552426a847e266af2d3ffaea3b522ae018
analyst <tshemake@gmail.com>
2018-01-14 17:59:06 +0500

    762/29604/step_4

---
+++ stepik/762/29604/step_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Сопоставьте имена и номера потоков ввода-вывода
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("stdout - 1");
+            Console.WriteLine("stdin - 0");
+            Console.WriteLine("stderr - 2");
+        }
+    }
+}
