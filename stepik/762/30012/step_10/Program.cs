13057498689c7f3355b6059062591c2d30f9b94d
analyst <tshemake@gmail.com>
2018-01-14 18:00:45 +0500

    762/30012/step_10

---
+++ stepik/762/30012/step_10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+/*
+ * Напишите результат выполнения команды:
+ *
+ * box@de98796:~$ echo послезавтра | sed 's/после/&&/'
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "послезавтра";
+            Console.WriteLine(Regex.Replace(str, "(после)", "$1$1"));
+        }
+    }
+}
