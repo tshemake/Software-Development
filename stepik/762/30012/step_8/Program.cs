47025366de5d9293a2daa27d017f380e5ffaa00c
analyst <tshemake@gmail.com>
2018-01-14 18:00:45 +0500

    762/30012/step_8

---
+++ stepik/762/30012/step_8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+/*
+ * Напишите результат выполнения команды:
+ *
+ *  box@de98796:~$ echo Заморский | sed 's/За/При/'
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex("За");
+            Console.WriteLine(regex.Replace("Заморский", "При", 1));
+        }
+    }
+}
