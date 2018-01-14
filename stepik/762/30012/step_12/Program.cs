0a7e252d0600ac52ec115fc1389b608ddb3af1dc
analyst <tshemake@gmail.com>
2018-01-14 18:00:45 +0500

    762/30012/step_12

---
+++ stepik/762/30012/step_12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * В директории /home/box/testdir дан набор тестовых файлов.
+ * 1. С помощью команды rename измените расширение у тестовых
+ * файлов с .text на .txt.
+ */
+
+namespace step_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("rename 's/.text/.txt/' testdir/*.text");
+        }
+    }
+}
