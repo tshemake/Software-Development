53ae7260118a4b961fa35927a10ebed9bcb0fb5b
analyst <tshemake@gmail.com>
2018-01-14 18:00:21 +0500

    762/29785/step_5

---
+++ stepik/762/29785/step_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Найти все директории, соответствующие шаблону 'foo*':
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("find / -type d -name 'foo*'");
+        }
+    }
+}
