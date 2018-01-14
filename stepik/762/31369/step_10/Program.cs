8ab36a7bd7d395d065f0b5b79b33302400d3b7df
analyst <tshemake@gmail.com>
2018-01-14 18:48:20 +0500

    762/31369/step_10

---
+++ stepik/762/31369/step_10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Какой пароль пользователя root задан по умолчанию в дистрибутивах
+ * Ubuntu/Xubuntu/Lubuntu?
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("пароль не задан");
+        }
+    }
+}
