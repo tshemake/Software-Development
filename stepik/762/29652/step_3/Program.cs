ccb77d9c153cab8602f0a773927c840427acea43
analyst <tshemake@gmail.com>
2018-01-14 17:59:51 +0500

    762/29652/step_3

---
+++ stepik/762/29652/step_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Какой фильтр перемещает данные из стандартного потока ввода
+ * stdin в стандартный поток вывода stdout, а также записывает их в
+ * файл; т.е. имеет два вывода?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tee");
+        }
+    }
+}
