cc5ecef0e6112a0d1b16aacb50f591cc2ae6f6b1
analyst <tshemake@gmail.com>
2018-01-14 18:10:31 +0500

    762/30662/step_4

---
+++ stepik/762/30662/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Отметьте корректные примеры she-bang
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#!/bin/bash");
+            Console.WriteLine("#!/bin/csh");
+        }
+    }
+}
