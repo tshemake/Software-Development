91e67d6c727092ec443877ed21b0a2713400e5f0
analyst <tshemake@gmail.com>
2018-01-14 18:01:07 +0500

    762/30070/step_3

---
+++ stepik/762/30070/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * С помощью какой клавиши можно перевести редактор vi  в режим
+ * ввода команд?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esc");
+        }
+    }
+}
