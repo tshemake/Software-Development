fe3c58ccec52ae2b8b96dff4e91ca29f723f700e
analyst <tshemake@gmail.com>
2018-01-14 23:17:44 +0500

    762/31390/step_5

---
+++ stepik/762/31390/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Сопоставьте утилиты с их функциями
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("usermod изменяет параметры учетных записей");
+            Console.WriteLine("useradd добавляет учетны записи пользователей в БД");
+            Console.WriteLine("userdel удаляет учетные записи пользователей");
+        }
+    }
+}
