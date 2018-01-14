6ddd9a785af67b868298fe53b7ef68f9bcafcb29
analyst <tshemake@gmail.com>
2018-01-14 23:49:22 +0500

    762/31406/step_4

---
+++ stepik/762/31406/step_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * С помощью каких утилит можно зашифровать ключевую фразу для
+ * создания пароля пользователя?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("passwd");
+            Console.WriteLine("openssl");
+        }
+    }
+}
