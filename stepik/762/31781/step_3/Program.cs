dd40eb5f868601b2c0e954a342cb4b77d2db6604
analyst <tshemake@gmail.com>
2018-01-15 01:05:54 +0500

    762/31781/step_3

---
+++ stepik/762/31781/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С помощью утилиты groupadd можно
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("создать новую группу");
+        }
+    }
+}
