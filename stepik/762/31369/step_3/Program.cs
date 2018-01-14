ffb21c510dc62bbb4c066b3162e4e3995097cf33
analyst <tshemake@gmail.com>
2018-01-14 18:48:19 +0500

    762/31369/step_3

---
+++ stepik/762/31369/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * С помощью каких команд можно узнать имя своей учетной записи?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("whoami");
+            Console.WriteLine("who am i");
+        }
+    }
+}
