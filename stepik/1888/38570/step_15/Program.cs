b686942dc6c753bac7079dcc8280aa7f28b94067
analyst <tshemake@gmail.com>
2018-01-16 00:03:02 +0500

    1888/38570/step_15

---
+++ stepik/1888/38570/step_15/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Выберите лишнее устройство
+ */
+
+namespace step_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("джойстик");
+        }
+    }
+}
