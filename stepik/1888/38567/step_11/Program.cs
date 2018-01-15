8867fa7f3b2742374e9f89e43f7f4ea03a23dac5
analyst <tshemake@gmail.com>
2018-01-15 23:27:22 +0500

    1888/38567/step_11

---
+++ stepik/1888/38567/step_11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Отметьте характеристики предметов и явлений, которые человек
+ * воспринимает при помощи органов слуха.
+ */
+
+namespace step_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Звонкий");
+            Console.WriteLine("Громкий");
+            Console.WriteLine("Ритмичный");
+            Console.WriteLine("Мелодичный");
+        }
+    }
+}
