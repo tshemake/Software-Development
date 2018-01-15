75b354a4d2f224d381edfd87b0be97eaf756ebc3
analyst <tshemake@gmail.com>
2018-01-16 00:03:02 +0500

    1888/38570/step_16

---
+++ stepik/1888/38570/step_16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Выберите лишнее устройство
+ */
+
+namespace step_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("монитор");
+        }
+    }
+}
