6b2957bfcef474280bd0363fc68a23e4d2a4711d
analyst <tshemake@gmail.com>
2018-01-15 23:55:03 +0500

    1888/38569/step_11

---
+++ stepik/1888/38569/step_11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Наука, занимающаяся изучением всевозможных способов передачи,
+ * хранения и обработки информации.
+ */
+
+namespace step_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Информатика");
+        }
+    }
+}
