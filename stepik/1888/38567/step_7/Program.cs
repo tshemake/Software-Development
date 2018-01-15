cc998c12ad877b66767a195d95c77ff8c1c00730
analyst <tshemake@gmail.com>
2018-01-15 23:27:20 +0500

    1888/38567/step_7

---
+++ stepik/1888/38567/step_7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Расставьте чувства человека в порядке увеличения количества
+ * получаемой с их помощью информации.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("вкус");
+            Console.WriteLine("осязание");
+            Console.WriteLine("обоняние");
+            Console.WriteLine("слух");
+            Console.WriteLine("зрение");
+        }
+    }
+}
