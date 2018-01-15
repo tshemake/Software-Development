520ddeac2466c166f4ca3a97c9f0febb3439c3c5
analyst <tshemake@gmail.com>
2018-01-15 23:49:42 +0500

    1888/38571/step_6

---
+++ stepik/1888/38571/step_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Укажите жизненные ситуации, в которых осуществляется передача
+ * информации.
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дедушка смотрит выпуск новостей по телевизору");
+            Console.WriteLine("Сын рассказывает родителям, как прошёл день в школе");
+            Console.WriteLine("Вы отправляете электронное письмо");
+        }
+    }
+}
