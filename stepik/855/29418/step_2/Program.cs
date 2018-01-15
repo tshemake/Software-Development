a29009f2392074f5ca5db95df36563d8005c5d86
analyst <tshemake@gmail.com>
2018-01-16 00:17:22 +0500

    855/29418/step_2

---
+++ stepik/855/29418/step_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} - {1}", 32, Convert.ToString(32, 2));
+            Console.WriteLine("{0} - {1}", 97, Convert.ToString(97, 2));
+            Console.WriteLine("{0} - {1}", 127, Convert.ToString(127, 2));
+            Console.WriteLine("{0} - {1}", 100, Convert.ToString(100, 2));
+            Console.WriteLine("{0} - {1}", 83, Convert.ToString(83, 2));
+        }
+    }
+}
