562ff44037e71390ad65b3664bb804c1e3e1e5b5
analyst <tshemake@gmail.com>
2018-01-15 23:55:03 +0500

    1888/38569/step_10

---
+++ stepik/1888/38569/step_10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Информация, представленная в форме, пригодной для обработки
+ * компьютером.
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Данные");
+        }
+    }
+}
