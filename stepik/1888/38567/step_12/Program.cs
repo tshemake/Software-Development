ff719c2e28217afb7def46792b30700b798947c5
analyst <tshemake@gmail.com>
2018-01-15 23:27:22 +0500

    1888/38567/step_12

---
+++ stepik/1888/38567/step_12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Отметьте характеристики предметов и явлений, которые человек
+ * воспринимает при помощи органов осязания.
+ */
+
+namespace step_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Мокрый");
+            Console.WriteLine("Мягкий");
+            Console.WriteLine("Горячий");
+            Console.WriteLine("Шершавый");
+        }
+    }
+}
