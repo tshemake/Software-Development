c6a994c268f6d735ae92be84194f9bd7f61dbeb7
analyst <tshemake@gmail.com>
2018-01-16 00:14:28 +0500

    855/29417/step_3

---
+++ stepik/855/29417/step_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Напишите пожалуйста в двоичной системе счисления числа с 8 по 15
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 8;
+            int max = 15;
+            for (int value = min; value <= max; value++)
+            {
+                Console.WriteLine(Convert.ToString(value, 2));
+            }
+        }
+    }
+}
