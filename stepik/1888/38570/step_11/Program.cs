da54accca8a6bbf90ebe3044717204fd4d72880b
analyst <tshemake@gmail.com>
2018-01-16 00:03:01 +0500

    1888/38570/step_11

---
+++ stepik/1888/38570/step_11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+ * Подпишите основные части компьютера.
+ */
+
+namespace step_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 - Принтер");
+            Console.WriteLine("2 - Акустические колонки");
+            Console.WriteLine("3 - Монитор");
+            Console.WriteLine("4 - Процессор");
+            Console.WriteLine("5 - Клавиатура");
+            Console.WriteLine("6 - Мышь");
+            Console.WriteLine("7 - Микросхемы памяти");
+            Console.WriteLine("8 - Жёсткий диск");
+            Console.WriteLine("9 - Дисковод гибких дисков");
+            Console.WriteLine("10 - Дисковод компакт-дисков");
+        }
+    }
+}
