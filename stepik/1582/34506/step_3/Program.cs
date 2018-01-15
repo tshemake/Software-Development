6351de81ff5db79cec66c89d7bf8bede699f8b1d
analyst <tshemake@gmail.com>
2018-01-16 00:07:19 +0500

    1582/34506/step_3

---
+++ stepik/1582/34506/step_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Исполнителем могут быть:
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Человек");
+            Console.WriteLine("Компьютер");
+            Console.WriteLine("Стиральная машина");
+            Console.WriteLine("Животное");
+        }
+    }
+}
