483f4fbc7cf0ed186d65e03ebf8ef25fe5857a00
analyst <tshemake@gmail.com>
2018-01-15 23:27:21 +0500

    1888/38567/step_9

---
+++ stepik/1888/38567/step_9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Определите вид информации в следующих ситуациях (установите
+ * соответствие)
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лиза пьёт горькую микстуру - Вкусовая");
+            Console.WriteLine("Ваня читает книгу - Зрительная");
+            Console.WriteLine("Витя ныряет в реку - Осязательная");
+            Console.WriteLine("Саша слушает радионовости - Звуковая");
+            Console.WriteLine("Вася вдыхает аромат свежеиспечённой булочки - Обонятельная");
+        }
+    }
+}
