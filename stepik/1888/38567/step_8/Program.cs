72dc1219d2d7f1028ad2cddb338b37a401d5eb08
analyst <tshemake@gmail.com>
2018-01-15 23:27:21 +0500

    1888/38567/step_8

---
+++ stepik/1888/38567/step_8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Сопоставьте животных с их наиболее развитыми чувствами.
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("волк - обоняние");
+            Console.WriteLine("летучая мышь - слух");
+            Console.WriteLine("крот - осязание");
+            Console.WriteLine("орёл - зрение");
+        }
+    }
+}
