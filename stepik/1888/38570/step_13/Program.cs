446bd23b00ee27cfe1eb96e31851d50b64739053
analyst <tshemake@gmail.com>
2018-01-16 00:03:02 +0500

    1888/38570/step_13

---
+++ stepik/1888/38570/step_13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Выберите лишнее устройство
+ */
+
+namespace step_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("акустические колонки");
+        }
+    }
+}
