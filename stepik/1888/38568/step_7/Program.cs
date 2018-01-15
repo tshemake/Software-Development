21d8c249f8dc1223bd11f59c0dbb3b30d272e4a6
analyst <tshemake@gmail.com>
2018-01-15 23:29:44 +0500

    1888/38568/step_7

---
+++ stepik/1888/38568/step_7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Сопоставьте информацию с формой её представления.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Возраст, рост и вес человека, записанные в его медицинской карте - Числовая");
+            Console.WriteLine("Мультипликационный фильм - Видео");
+            Console.WriteLine("Стихотворение в учебнике - Текстовая");
+            Console.WriteLine("Радиопередача - Звуковая");
+            Console.WriteLine("Картина в музее - Графическая");
+        }
+    }
+}
