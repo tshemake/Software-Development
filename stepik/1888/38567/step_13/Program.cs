b668417c44fded95f4e5cae7491b31d770e8c9e4
analyst <tshemake@gmail.com>
2018-01-15 23:27:22 +0500

    1888/38567/step_13

---
+++ stepik/1888/38567/step_13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Отметьте характеристики предметов и явлений, которые человек
+ * воспринимает при помощи органов вкуса.
+ */
+
+namespace step_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Горький");
+            Console.WriteLine("Сладкий");
+            Console.WriteLine("Солёный");
+            Console.WriteLine("Кислый");
+        }
+    }
+}
