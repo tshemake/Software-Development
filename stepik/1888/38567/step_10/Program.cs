f818c25692f1a8ccfdd7b443353c4e2f7c41f354
analyst <tshemake@gmail.com>
2018-01-15 23:27:22 +0500

    1888/38567/step_10

---
+++ stepik/1888/38567/step_10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Отметьте характеристики предметов и явлений, которые человек
+ * воспринимает при помощи органов зрения.
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Светлый");
+            Console.WriteLine("Яркий");
+            Console.WriteLine("Тусклый");
+            Console.WriteLine("Огромный");
+            Console.WriteLine("Широкий");
+            Console.WriteLine("Близкий");
+        }
+    }
+}
