a0ffe3b09e10f7745d177685c875052aee2130eb
analyst <tshemake@gmail.com>
2018-01-15 23:27:22 +0500

    1888/38567/step_14

---
+++ stepik/1888/38567/step_14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Отметьте характеристики предметов и явлений, которые человек
+ * воспринимает при помощи органов обоняния.
+ */
+
+namespace step_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Морской");
+            Console.WriteLine("Свежий");
+            Console.WriteLine("Цветочный");
+            Console.WriteLine("Мускатный");
+            Console.WriteLine("Ароматный");
+        }
+    }
+}
