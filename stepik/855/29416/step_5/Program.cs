d898d0c15b5f3fa1eb9ef45b45d4e448f0026927
analyst <tshemake@gmail.com>
2018-01-16 00:11:32 +0500

    855/29416/step_5

---
+++ stepik/855/29416/step_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * В какой системе счисления может быть записано число 12212?
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("В троичной");
+        }
+    }
+}
