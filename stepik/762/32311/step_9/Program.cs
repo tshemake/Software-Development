df3be9943ae7cd197ffe93c1079195bcdcef1c8b
analyst <tshemake@gmail.com>
2018-01-16 21:01:00 +0500

    762/32311/step_9

---
+++ stepik/762/32311/step_9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Сопоставьте тип файла и соответствующий ему символ в выводе ls -l
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("b - Блочное устройство");
+            Console.WriteLine("l - Символьная ссылка");
+            Console.WriteLine("p - Именованный канал");
+            Console.WriteLine("c - Символьное устройство");
+            Console.WriteLine("s - Сокет");
+            Console.WriteLine("- - Обычный файл");
+            Console.WriteLine("d - Директория");
+        }
+    }
+}
