6748aa8524d2815376d59e9f60cfbd569e93c7c2
analyst <tshemake@gmail.com>
2018-01-16 21:00:59 +0500

    762/32311/step_3

---
+++ stepik/762/32311/step_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * С помощью утилиты chown можно изменить
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Имя группы, владеющей файлом");
+            Console.WriteLine("Имя пользователся, владеющего файлом");
+        }
+    }
+}
