8a8eb55ddf19dd1dc84c36e17868b7c419e5ae63
analyst <tshemake@gmail.com>
2018-01-16 21:00:59 +0500

    762/32311/step_4

---
+++ stepik/762/32311/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С помощью утилиты chgrp можно изменить
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Имя группы, владеющей файлом");
+        }
+    }
+}
