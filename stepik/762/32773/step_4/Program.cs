1ab5dfa01eda49dae17379938886f263ea43b065
analyst <tshemake@gmail.com>
2018-01-16 21:39:52 +0500

    762/32773/step_4

---
+++ stepik/762/32773/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Чтобы посмотреть идентификаторы структур inode для файлов нужно
+ * выполнить
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ls -li");
+        }
+    }
+}
