19508563b23c3e7c8b3fd4cc6db92053c1777784
analyst <tshemake@gmail.com>
2018-01-16 21:02:19 +0500

    762/32436/step_3

---
+++ stepik/762/32436/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Бит sticky для директории нужен
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("для предотвращения удаления файлов пользователями, которые не являются их непосредственными владельцами.");
+        }
+    }
+}
