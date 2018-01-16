f1ea3b671a4d0f32ef1817b669b222a442fb9dc7
analyst <tshemake@gmail.com>
2018-01-16 21:43:14 +0500

    762/32801/step_4

---
+++ stepik/762/32801/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Сопоставьте утилиты с их функциями
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lsusb выводит список устройств usb");
+            Console.WriteLine("lspci выводит список устройств pci");
+        }
+    }
+}
