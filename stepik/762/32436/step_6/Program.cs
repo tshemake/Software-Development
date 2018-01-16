4e8b3f5dec488649a3cdf8ed534449968297ebea
analyst <tshemake@gmail.com>
2018-01-16 21:02:19 +0500

    762/32436/step_6

---
+++ stepik/762/32436/step_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+ * 1. Создайте группу sports. Поместите в группу sports
+ * пользователей foo и bar.
+ *
+ * 2. Создайте директорию ~/sportsdir, которая будет принадлежать
+ * группе пользователей sports.  Члены группы пользователей sports должны иметь возможность создавать файлы в данной директории. Все файлы, создаваемые в данной директории, должны принадлежать группе пользователей sports. Пользователи должны иметь возможность удалять из данной директории только принадлежащие им файлы.
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sudo groupadd sports");
+            Console.WriteLine("sudo usermod -a -G sports foo");
+            Console.WriteLine("sudo usermod -a -G sports bar");
+            Console.WriteLine("mkdir -m g+s,+t sportsdir");
+            Console.WriteLine("sudo chgrp sports sportsdir");
+        }
+    }
+}
