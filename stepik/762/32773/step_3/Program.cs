d5ed349137e83ac0a6fd3cc2a11d55493eab40e2
analyst <tshemake@gmail.com>
2018-01-16 21:39:52 +0500

    762/32773/step_3

---
+++ stepik/762/32773/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Inode является структурой данных, содержащей
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("относящиеся к файлу метаданные");
+        }
+    }
+}
