c090b656187000381d380d01004ad200ad7526f2
analyst <tshemake@gmail.com>
2018-01-16 21:01:00 +0500

    762/32311/step_7

---
+++ stepik/762/32311/step_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Напишите команду, которая заменит группу владельца каталога
+ * /home/foo на bar.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chgrp bar /home/foo");
+        }
+    }
+}
