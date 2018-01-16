6f5e14edeaf4b8e94c36e4e6bc32baa5d048e95a
analyst <tshemake@gmail.com>
2018-01-16 21:04:43 +0500

    762/32554/step_4

---
+++ stepik/762/32554/step_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * С помощью утилиты setfacl может осуществляться
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запись списков контроля доступа");
+            Console.WriteLine("Модификация списков контроля доступа");
+            Console.WriteLine("Удаление элементов списков контроля доступа");
+            Console.WriteLine("Удаление списков контроля доступа");
+        }
+    }
+}
