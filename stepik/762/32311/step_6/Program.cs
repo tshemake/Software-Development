bd9b3a362c7b392809455cfcdd55f6ccc9845f9b
analyst <tshemake@gmail.com>
2018-01-16 21:01:00 +0500

    762/32311/step_6

---
+++ stepik/762/32311/step_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Напишите команду, которая заменит пользователя владельца каталога
+ * /home/foo на bar.
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chown bar /home/foo");
+        }
+    }
+}
