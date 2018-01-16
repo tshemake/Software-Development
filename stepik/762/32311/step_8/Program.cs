b44dd5cb1366e833098c5cae27d75922e0ed91eb
analyst <tshemake@gmail.com>
2018-01-16 21:01:00 +0500

    762/32311/step_8

---
+++ stepik/762/32311/step_8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Напишите команду, которая заменит пользователя и группу владельца
+ * каталога /home/foo на bar и baz соотвественно.
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chown bar:baz /home/foo");
+        }
+    }
+}
