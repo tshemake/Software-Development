ce80e0f9d4832144f28d7e0d0f0990e14ac9fb56
analyst <tshemake@gmail.com>
2018-01-16 21:04:43 +0500

    762/32554/step_3

---
+++ stepik/762/32554/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * С помощью утилиты getfacl может осуществляться
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Чтение списков контроля доступа");
+        }
+    }
+}
