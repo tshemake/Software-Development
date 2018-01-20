ef0d3352c378e4d445c2d9afb56b25f111d33258
analyst <tshemake@gmail.com>
2018-01-21 00:35:38 +0500

    3078/38673/step_4

---
+++ stepik/3078/38673/step_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Выберите варианты с корректными объявлениями, присваиваниями.
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("int k = 2, b, c=-4;");
+            Console.WriteLine("double x = 17.32, y, z = 1;");
+            Console.WriteLine("char symbol = 'c';");
+        }
+    }
+}
