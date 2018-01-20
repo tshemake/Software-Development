6c1705df3464390a19529544f7d795eea59e29a4
analyst <tshemake@gmail.com>
2018-01-20 21:42:20 +0500

    3577/57858/step_9

---
+++ stepik/3577/57858/step_9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Переведите число 111001 из двоичной системы счисления в
+ * восьмеричную. Запишите ответ в восьмеричной системе счисления.
+ * Основание системы счисления писать не нужно
+ */
+
+namespace step_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} - {1}", "111001", Convert.ToString(Convert.ToInt32("111001", 2), 8));
+        }
+    }
+}
