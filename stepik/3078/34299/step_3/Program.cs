ead5da6229ff7c0cb54e4bd8c351311fde6b582c
analyst <tshemake@gmail.com>
2018-01-20 23:39:17 +0500

    3078/34299/step_3

---
+++ stepik/3078/34299/step_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Отметьте варианты данных, для которых лучше использовать тип int
+ * (целые числа).
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("количество книг в библиотеке");
+            Console.WriteLine("количество посетителей страницы за сутки");
+            Console.WriteLine("сумма двух целых чисел");
+            Console.WriteLine("произведение двух целых чисел");
+            Console.WriteLine("количество букв в слове");
+            Console.WriteLine("вес человека (кг.), полученный с механических напольных весов");
+        }
+    }
+}
