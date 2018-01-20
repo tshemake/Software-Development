6626d8ce9bb81a5e0c29fea2bd0f8a03117cff1e
analyst <tshemake@gmail.com>
2018-01-20 21:17:32 +0500

    3577/54916/step_15

---
+++ stepik/3577/54916/step_15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Переведите число 169 из де­ся­тич­ной си­сте­мы счис­ле­ния в дво­ич­ную.
+ * Сколь­ко нулей со­дер­жит по­лу­чен­ное число? В от­ве­те ука­жи­те одно
+ * число — ко­ли­че­ство значащих нулей.
+ */
+
+namespace step_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Convert.ToString(169, 2).Split('0').Length - 1);
+        }
+    }
+}
