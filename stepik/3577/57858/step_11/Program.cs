d440fe720fc2f334d5ab84e12876fdc1a1a35d5b
analyst <tshemake@gmail.com>
2018-01-20 21:42:20 +0500

    3577/57858/step_11

---
+++ stepik/3577/57858/step_11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Переведите число 307 из восьмеричной системы счисления в
+ * двоичную. Запишите ответ в двоичной системе счисления. Основание
+ * системы счисления писать не нужно.
+ */
+
+namespace step_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} - {1}", "307", Convert.ToString(Convert.ToInt32("307", 8), 2));
+        }
+    }
+}
