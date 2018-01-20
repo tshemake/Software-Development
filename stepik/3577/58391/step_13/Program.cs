8d61ead8369d23e620f31e0911d922ab41912dfb
analyst <tshemake@gmail.com>
2018-01-20 22:04:41 +0500

    3577/58391/step_13

---
+++ stepik/3577/58391/step_13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Переведите число BF из шестнадцатеричной системы счисления в
+ * двоичную. Запишите ответ в двоичной системе счисления. Основание
+ * системы счисления писать не нужно.
+ */
+
+namespace step_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0:X} - {1}", 0xBF, Convert.ToString(0xBF, 2));
+        }
+    }
+}
