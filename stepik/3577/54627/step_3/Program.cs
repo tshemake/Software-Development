7a9a2f6fa64ff547043378021c391325bc9ba856
analyst <tshemake@gmail.com>
2018-01-20 20:56:17 +0500

    3577/54627/step_3

---
+++ stepik/3577/54627/step_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Знаки, с помощью которых записываются числа.
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Цифры");
+        }
+    }
+}
