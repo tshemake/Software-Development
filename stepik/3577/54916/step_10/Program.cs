9a0da728be94979e938ea483eb8a605679db4549
analyst <tshemake@gmail.com>
2018-01-20 21:17:32 +0500

    3577/54916/step_10

---
+++ stepik/3577/54916/step_10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Найдите сумму двоичных чисел 111000 + 1010011 . В ответе запишите
+ * двоичное число. Основание системы счисления писать не нужно.
+ */
+
+namespace step_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "111000";
+            string b = "1010011";
+            Console.WriteLine("{0} + {1} = {2}", a, b, Convert.ToString(Convert.ToInt32(a, 2) + Convert.ToInt32(b, 2), 2));
+        }
+    }
+}
