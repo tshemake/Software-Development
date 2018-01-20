50a7abf41276fc7e9548f791c4ebcedf3c00f8a8
analyst <tshemake@gmail.com>
2018-01-20 21:42:20 +0500

    3577/57858/step_8

---
+++ stepik/3577/57858/step_8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Сопоставьте числа в восьмеричной и двоичной системах счисления
+ * (первый столбик - запись числа в восьмеричной системе счисления,
+ * второй - запись числа в двоичной системе счисления).
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= Convert.ToInt32("7", 8); i++)
+            {
+                Console.WriteLine("{0} - {1}", Convert.ToString(i, 8), Convert.ToString(i, 2));
+            }
+        }
+    }
+}
