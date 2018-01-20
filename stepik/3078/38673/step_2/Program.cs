f03e7db9729ceacd958723a0d162224e83961fe3
analyst <tshemake@gmail.com>
2018-01-21 00:35:37 +0500

    3078/38673/step_2

---
+++ stepik/3078/38673/step_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+ * Чему равны значения переменных x,y после выполнения данного
+ * куска программы:
+ *
+ *  int x=2, y=7;
+ *  x = y;
+ *  y = x;
+ * Попробуйте сначала угадать, а потом проверьте своё предположение.
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2, y = 7;
+            x = y;
+            y = x;
+            Console.WriteLine("x={0}, y={1}", x, y);
+        }
+    }
+}
