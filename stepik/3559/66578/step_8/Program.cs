dfff232ca87c0583d6cd50b6c40b1b5e8f40d8c6
analyst <tshemake@gmail.com>
2018-01-20 17:28:41 +0500

    3559/66578/step_8

---
+++ stepik/3559/66578/step_8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Запишите, что выведет интерактивный интерпретатор языка
+ * программирования Питон, выполнив следующее выражение:
+ * print('2 + 2 =', 2 + 2)
+ */
+
+namespace step_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 + 2 = {0}", 2 + 2);
+        }
+    }
+}
