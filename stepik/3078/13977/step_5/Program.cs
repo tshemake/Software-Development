d2536fbd5656c1847f4a36699cce545710ce9621
analyst <tshemake@gmail.com>
2018-01-20 22:49:06 +0500

    3078/13977/step_5

---
+++ stepik/3078/13977/step_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * Напишите программу, которая выводит на экран следующие строки:
+ *
+ *  Hello, World!
+ *  The world is yours!
+ * Sample Input:
+ *
+ *
+ * Sample Output:
+ *
+ *  Hello, World!
+ *  The world is yours!
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("The world is yours!");
+        }
+    }
+}
---
+++ stepik/3078/13977/step_5/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\nThe world is yours!");
+    return 0;
+}
