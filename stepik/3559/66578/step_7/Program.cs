05b3d565358d7fe403cfb4033275757e31d4056e
analyst <tshemake@gmail.com>
2018-01-20 17:28:41 +0500

    3559/66578/step_7

---
+++ stepik/3559/66578/step_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+/*
+ * Запишите, что выведет интерактивный интерпретатор языка
+ * программирования Питон, выполнив следующее выражение:
+ * 'АБВГДЕйка' * 7
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Concat(Enumerable.Repeat("АБВГДЕйка", 7)));
+        }
+    }
+}
