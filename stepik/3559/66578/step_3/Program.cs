d618ef8a645d363bab167ae996b13c781c814030
analyst <tshemake@gmail.com>
2018-01-20 17:28:41 +0500

    3559/66578/step_3

---
+++ stepik/3559/66578/step_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+/*
+ * Вычислите значение выражения в интерактивном интерпретаторе языка
+ * программирования Питон:
+ * 999888777666555444333222111 - 111222333444555666777888999
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BigInteger.Parse("999888777666555444333222111") - BigInteger.Parse("111222333444555666777888999"));
+        }
+    }
+}
