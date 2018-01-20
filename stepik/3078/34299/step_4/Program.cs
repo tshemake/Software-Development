283ce4c671597202f44c8411d01d1524377e2561
analyst <tshemake@gmail.com>
2018-01-20 23:39:17 +0500

    3078/34299/step_4

---
+++ stepik/3078/34299/step_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Отметьте варианты данных, для которых лучше использовать тип
+ * double или float (вещественные числа).
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("результат деления одного целого числа, на другое");
+            Console.WriteLine("стоимость товара");
+            Console.WriteLine("вес человека (кг.), полученный с электронных напольных весов");
+        }
+    }
+}
