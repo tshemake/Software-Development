d849a1967cf5c1a831449a61739eaa60ebe47678
analyst <tshemake@gmail.com>
2018-01-20 23:58:37 +0500

    3078/36170/step_5

---
+++ stepik/3078/36170/step_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Отметьте объявления с ошибками:
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x, y, z int;");
+            Console.WriteLine("INT a;");
+            Console.WriteLine("integer v;");
+        }
+    }
+}
