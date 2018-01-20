65e94849d5de95dbfbcdc1407e99289cb96e0da3
analyst <tshemake@gmail.com>
2018-01-21 00:35:38 +0500

    3078/38673/step_5

---
+++ stepik/3078/38673/step_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ * Чему равно значение переменной x после выполнения следующей
+ * программы:
+ *
+ *  #include <stdio.h>
+ *  int main(void){
+ *  int n = 2;
+ *  double x=0, pi = 3.14;
+ *
+ *  x = n*pi*x;
+ *
+ *  return 0;
+ *  }
+ */
+
+namespace step_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 2;
+            double x = 0, pi = 3.14;
+            x = n * pi * x;
+            Console.WriteLine(x);
+        }
+    }
+}
