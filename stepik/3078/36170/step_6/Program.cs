28d83bc5a3aa7ea389fa34b113e8d658ac959527
analyst <tshemake@gmail.com>
2018-01-20 23:58:37 +0500

    3078/36170/step_6

---
+++ stepik/3078/36170/step_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Заполните пропуски в данной программе:
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"#include <stdio.h>
+int main (void) {
+int number; // number переменная типа int
+return (0);
+}");
+        }
+    }
+}
