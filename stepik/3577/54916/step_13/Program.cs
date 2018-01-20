8d39a7f9bcdd4385a67c0e6a171c541012c22dbe
analyst <tshemake@gmail.com>
2018-01-20 21:17:32 +0500

    3577/54916/step_13

---
+++ stepik/3577/54916/step_13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Найдите разность двоичных чисел 10101010 - 1001110. В ответе
+ * запишите двоичное число. Основание системы счисления писать не
+ * нужно.
+ */
+
+namespace step_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "10101010";
+            string b = "1001110";
+            Console.WriteLine("{0} - {1} = {2}", a, b, Convert.ToString(Convert.ToInt32(a, 2) - Convert.ToInt32(b, 2), 2));
+        }
+    }
+}
