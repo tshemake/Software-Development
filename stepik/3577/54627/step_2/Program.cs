1e704971eb008da4912e2a97d778289a37d24bff
analyst <tshemake@gmail.com>
2018-01-20 20:56:17 +0500

    3577/54627/step_2

---
+++ stepik/3577/54627/step_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Знаковая система, в которой приняты определённые правила записи
+ * чисел.
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Система счисления");
+        }
+    }
+}
