2dbeeee76526e2ad7eb1517fc7cf6279a965eca9
analyst <tshemake@gmail.com>
2018-01-20 21:17:18 +0500

    3577/54916/step_4

---
+++ stepik/3577/54916/step_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Числу, записанному в десятичной системе счисления, впишите
+ * соответствующее число, записанное в двоичной системе счисления.
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 11; i <= 20; i++)
+            {
+                Console.WriteLine("{0} - {1}", i, Convert.ToString(i, 2));
+            }
+        }
+    }
+}
