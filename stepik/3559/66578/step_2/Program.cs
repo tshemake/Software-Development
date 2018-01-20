7a5575d4a444f95d974e6e2d8eb8f93b73aed1d3
analyst <tshemake@gmail.com>
2018-01-20 17:28:41 +0500

    3559/66578/step_2

---
+++ stepik/3559/66578/step_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+/*
+ * Вычислите значение выражения в интерактивном интерпретаторе языка
+ * программирования Питон:
+ * 12345678987654321 + 98765432123456789
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BigInteger.Parse("12345678987654321") + BigInteger.Parse("98765432123456789"));
+        }
+    }
+}
