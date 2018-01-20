95b1f78b4d808ae6b9b19ac82a4cec5c839cb8cf
analyst <tshemake@gmail.com>
2018-01-20 23:58:37 +0500

    3078/36170/step_2

---
+++ stepik/3078/36170/step_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Отметьте правильные имена переменных.
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("b2b");
+            Console.WriteLine("number");
+            Console.WriteLine("ImyaPeremennoy");
+            Console.WriteLine("summa_a_b");
+        }
+    }
+}
