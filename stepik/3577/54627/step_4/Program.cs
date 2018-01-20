79d16488832968df16b4e4f54c4aa34f1225deb7
analyst <tshemake@gmail.com>
2018-01-20 20:56:17 +0500

    3577/54627/step_4

---
+++ stepik/3577/54627/step_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Совокупность знаков системы счисления.
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Алфавит");
+        }
+    }
+}
