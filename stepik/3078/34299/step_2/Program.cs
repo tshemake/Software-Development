fe58b06d6581a0b458793d2ad97370cbbbebdb63
analyst <tshemake@gmail.com>
2018-01-20 23:39:17 +0500

    3078/34299/step_2

---
+++ stepik/3078/34299/step_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Сопоставьте типы данных и значения так, чтобы каждому значению
+ * соответствовал допустимый тип данных.
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("char - 'd'");
+            Console.WriteLine("int - 1861");
+            Console.WriteLine("double - 3.1415926");
+        }
+    }
+}
