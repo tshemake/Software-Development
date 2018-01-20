5cd1b8bc6f7ff654d1e2652ccd2d7d271529479e
analyst <tshemake@gmail.com>
2018-01-20 22:49:05 +0500

    3078/13977/step_4

---
+++ stepik/3078/13977/step_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Расставьте строки в правильном порядке, чтобы получилась рабочая
+ * программа.
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#include <stdio.h>");
+            Console.WriteLine("int main(void) {");
+            Console.WriteLine("printf(\"Hello, World!\");");
+            Console.WriteLine("return 0;");
+            Console.WriteLine("}");
+        }
+    }
+}
