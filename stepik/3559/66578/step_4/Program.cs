dcac42adce93e6c231d91d84728a000a9120fa4a
analyst <tshemake@gmail.com>
2018-01-20 17:28:41 +0500

    3559/66578/step_4

---
+++ stepik/3559/66578/step_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+/*
+ * Вычислите значение выражения в интерактивном интерпретаторе языка
+ * программирования Питон:
+ * 77777777777777777 * 77777777777777777
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BigInteger.Parse("77777777777777777") * BigInteger.Parse("77777777777777777"));
+        }
+    }
+}
