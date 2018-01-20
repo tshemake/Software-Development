3755ab3b6561a4c8084f123c707669a1bb46da89
analyst <tshemake@gmail.com>
2018-01-20 22:49:05 +0500

    3078/13977/step_2

---
+++ stepik/3078/13977/step_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Напишите строку программы с директивой препроцессора для
+ * подключения файла conio.h
+ */
+
+namespace step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#include <conio.h>");
+        }
+    }
+}
