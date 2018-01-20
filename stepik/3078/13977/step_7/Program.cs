f5fce0bbae686271bc1e66c2bedc92411655f91b
analyst <tshemake@gmail.com>
2018-01-20 22:49:06 +0500

    3078/13977/step_7

---
+++ stepik/3078/13977/step_7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/*
+ * Напишите программу, которая выводит на экран следующие строки:
+ *
+ *  NICKNAME: Neo
+ *  CITY: St-Petersburg
+ *  AGE: 35
+ *  HEIGHT: 180
+ *  WEIGHT: 75
+ * Sample Input:
+ *
+ *
+ * Sample Output:
+ *
+ *  NICKNAME: Neo
+ *  CITY: St-Petersburg
+ *  AGE: 35
+ *  HEIGHT: 180
+ *  WEIGHT: 75
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"NICKNAME: Neo
+CITY: St-Petersburg
+AGE: 35
+HEIGHT: 180
+WEIGHT: 75");
+        }
+    }
+}
