172242215b4c1e1ae17b2b97434f356bfae24ae3
analyst <tshemake@gmail.com>
2018-01-20 21:17:32 +0500

    3577/54916/step_7

---
+++ stepik/3577/54916/step_7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Переведи число 109 из десятичной системы счисления в двоичную.
+ * В ответе запишите полученное двоичное число. Основание системы
+ * счисления писать не нужно.
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} - {1}", 109, Convert.ToString(109, 2));
+        }
+    }
+}
