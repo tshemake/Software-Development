e32767deb6f40a9955fc3005567d8b7a474d2c3a
analyst <tshemake@gmail.com>
2018-01-20 22:49:06 +0500

    3078/13977/step_6

---
+++ stepik/3078/13977/step_6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/*
+ * Вы смотрели фильм «Матрица»? Если нет, то советую посмотреть.
+ * А задание будет следующим: переписать программу «Hello, world!»
+ * так, чтобы на экране появился следующий текст:
+ *
+ *  Wake up, Neo...
+ *
+ *  The Matrix has you...
+ *
+ *  Follow the white rabbit...
+ * Подсказка: Вы можете использовать сколько угодно символов "\n".
+ * Строки с текстом разделены пустой строкой.
+ *
+ * Sample Input:
+ *
+ *
+ * Sample Output:
+ *
+ *  Wake up, Neo...
+ *
+ *  The Matrix has you...
+ *
+ *  Follow the white rabbit...
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wake up, Neo...\n");
+            Console.WriteLine("The Matrix has you...\n");
+            Console.WriteLine("Follow the white rabbit...");
+        }
+    }
+}
