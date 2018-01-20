1b36ba5c28d4e8763bb691f01bbd44a51d51b58f
analyst <tshemake@gmail.com>
2018-01-21 00:35:38 +0500

    3078/38673/step_7

---
+++ stepik/3078/38673/step_7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * Отметьте строки программы, в которых допущены ошибки :
+ *
+ *  #include <stdio.h> 
+ *  int main(void) {
+ *  	INT a, d;
+ *  	б = 2;
+ *  	double k, sum;
+ *  	cum = 23.4; 
+ *  	return 0;
+ *  }
+ */
+
+namespace step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("6");
+        }
+    }
+}
