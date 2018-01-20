8561c0bc0e28ee50ac670981e19711a7f2591705
analyst <tshemake@gmail.com>
2018-01-20 23:58:37 +0500

    3078/36170/step_3

---
+++ stepik/3078/36170/step_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * Отметьте неправильные имена переменных:
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number_Васи");
+            Console.WriteLine("line-2");
+            Console.WriteLine("переменная");
+            Console.WriteLine("2number");
+            Console.WriteLine("mac-address");
+        }
+    }
+}
