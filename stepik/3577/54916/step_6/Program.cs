3d0b405e4d3d60649eeda19ad1e7866f45f71cf3
analyst <tshemake@gmail.com>
2018-01-20 21:17:32 +0500

    3577/54916/step_6

---
+++ stepik/3577/54916/step_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Переведи число 112 из десятичной системы счисления в двоичную.
+ * В ответе запишите полученное двоичное число. Основание системы
+ * счисления писать не нужно.
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} - {1}", 112, Convert.ToString(112, 2));
+        }
+    }
+}
