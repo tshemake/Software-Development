71182cbbf8adaf1028b6c69022936504853cf5cb
analyst <tshemake@gmail.com>
2018-01-20 16:51:05 +0500

    3559/54502/step_13

---
+++ stepik/3559/54502/step_13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+ * В забеге шести спортсменов Андрей отстал от Бориса и еще от двух
+ * спортсменов. Виктор финишировал после Дмитрия, но ранее Геннадия.
+ * Дмитрий опередил Бориса, но все же пришел после Евгения. Какое
+ * место занял каждый спортсмен?
+ */
+
+namespace step_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I - Евгений");
+            Console.WriteLine("II - Дмитрий");
+            Console.WriteLine("III - Борис");
+            Console.WriteLine("IV - Андрей");
+            Console.WriteLine("V - Виктор");
+            Console.WriteLine("VI - Геннадий");
+        }
+    }
+}
