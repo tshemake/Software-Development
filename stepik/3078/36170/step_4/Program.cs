f75272997a13bfb14365021a996dd930263bbf50
analyst <tshemake@gmail.com>
2018-01-20 23:58:37 +0500

    3078/36170/step_4

---
+++ stepik/3078/36170/step_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+ * Сопоставьте объявления и описания.
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("int N; объявление целочисленной переменной с именем N");
+            Console.WriteLine("double x,y; объявление двух вещественных переменных с именами x и y");
+            Console.WriteLine("int x, y; объявление двух целочисленных переменных с именами x и y");
+            Console.WriteLine("char symbol; объявление переменной символьного типа с именем symbol");
+        }
+    }
+}
