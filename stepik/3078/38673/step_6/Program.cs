c871034a141402044fdef8c449420ed8300c2fae
analyst <tshemake@gmail.com>
2018-01-21 00:35:38 +0500

    3078/38673/step_6

---
+++ stepik/3078/38673/step_6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+ * Расставьте строки программы так, чтобы получилась синтаксически
+ * правильная программа, после выполнения которой в переменной x
+ * будет значение 10, а в переменной y значение 100.
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"#include <stdio.h>
+int main (void) {
+int x=0, y=0;
+x=100;
+y = x;
+x = 10;
+return 0;
+}");
+        }
+    }
+}
