7780fb9b14f184f38f5fa40962f21a1dd22f8964
analyst <tshemake@gmail.com>
2018-01-21 18:19:16 +0500

    2911/46756/step_4

---
+++ stepik/2911/46756/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Определите, всегда ли верно равенство
+ * P\left( {\overline {{\rm{A}} \cap {\rm{B}} \cap {\rm{C}}} } \right) = P\left( {\overline {\rm{C}} } \right) + P\left( {\overline {\rm{A}}  \cap C} \right) + P\left( {{\rm{A}} \cap \overline {\rm{B}}  \cap {\rm{C}}} \right)?
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("да");
+        }
+    }
+}
