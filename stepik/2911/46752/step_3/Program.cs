ccb2be0ff6abf851b2e4b33e14a413d4c9ea614c
analyst <tshemake@gmail.com>
2018-01-21 17:17:46 +0500

    2911/46752/step_3

---
+++ stepik/2911/46752/step_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Какая формула верно описывает заштрихованное событие? Отметьте
+ * все подходящие.
+ *  а) (A \cap C)\setminus(A \cap B \cap C)
+ *  б) A \cap \bar B \cap C
+ *  в) (A \cap C)\setminus B
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("а)");
+            Console.WriteLine("б)");
+            Console.WriteLine("в)");
+        }
+    }
+}
