0e3291eb561cec8a605d0d64b05f166782c64d11
analyst <tshemake@gmail.com>
2018-01-21 17:49:08 +0500

    2911/46755/step_6

---
+++ stepik/2911/46755/step_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Определите, всегда ли верно равенство
+ *
+ * \overline B \cap \overline {A \cup C} = \overline {A \cup B \cup C}   
+ */
+
+namespace step_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("да");
+        }
+    }
+}
