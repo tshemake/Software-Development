74f888221aadebd78aacdf13eac43d8611d31788
analyst <tshemake@gmail.com>
2018-01-21 17:49:08 +0500

    2911/46755/step_3

---
+++ stepik/2911/46755/step_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Определите, всегда ли верно равенство
+ *
+ * \overline {A \cap B \cap C}  = \overline {B \cap C}  \cup \overline A  
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("да");
+        }
+    }
+}
