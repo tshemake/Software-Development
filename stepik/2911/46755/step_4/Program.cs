4e7794fdbfdda88651add0dd39feea1109c6d5ee
analyst <tshemake@gmail.com>
2018-01-21 17:49:08 +0500

    2911/46755/step_4

---
+++ stepik/2911/46755/step_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * Определите, всегда ли верно равенство
+ * \Omega \backslash \overline {\overline A \cup \overline B \cup \overline C } = \overline {A \cap B \cap C}
+ */
+
+namespace step_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("да");
+        }
+    }
+}
