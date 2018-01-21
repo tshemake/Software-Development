409bb749e168aa8a3a9786374c2ddd7aed6abf1b
analyst <tshemake@gmail.com>
2018-01-21 16:23:45 +0500

    2911/46750/step_3

---
+++ stepik/2911/46750/step_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * Одновременно бросаем монету и игральную кость (кубик).
+ * Сколько элементарных исходов будет содержать выборочное
+ * пространство \Omega?
+ */
+
+namespace step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|Ω|=12");
+        }
+    }
+}
